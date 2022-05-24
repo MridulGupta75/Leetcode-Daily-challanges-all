82360f1052bc7ac5ddf34ed18abd6672eb56f859
MridulGupta75 <66024896+MridulGupta75@users.noreply.github.com>
2022-05-24 21:18:08 +0530

    Added solution - LeetHub

---
+++ Anagram Palindrome - GFG/anagram-palindrome.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int isPossible (string s);
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		string s; cin >> s;
+		if (isPossible (s))
+			cout << "Yes\n";
+		else
+			cout << "No\n";
+	}
+}
+
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+int isPossible (string S)
+{
+    map<char,int>mpp;
+    for(auto i:S)
+    {
+        map<char,int>::iterator it;
+        it=mpp.find(i);
+        if(it!=mpp.end())
+        {
+            it->second=(it->second)+1;
+        }
+        else
+        mpp.insert({i,1});
+    }
+    int odd=0;
+    for(auto j:mpp)
+    {
+        if((j.second)%2!=0)odd++;
+        
+    }
+    if(odd>1)return 0;
+    return 1;
+}
