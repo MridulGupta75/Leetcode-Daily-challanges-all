96f519322778e7a7937febbd4100da4b8f16eb21
MridulGupta75 <66024896+MridulGupta75@users.noreply.github.com>
2022-05-24 20:42:36 +0530

    Added solution - LeetHub

--- Check if possible to move from given coordinate to desired coordinate - GFG/check-if-possible-to-move-from-given-coordinate-to-desired-coordinate.cpp
+++ Check if possible to move from given coordinate to desired coordinate - GFG/check-if-possible-to-move-from-given-coordinate-to-desired-coordinate.cpp
@@ -9,17 +9,17 @@ using namespace std;
 
 class Solution {
   public:
-//   int gcd(int a,int b)
-//   {
-//       if(b==0)return a;
-//       return gcd(a,a%b);
-//   }
+  int gcd(int a,int b)
+  {
+      if(b==0)return a;
+      return gcd(b,a%b);
+  }
     int isPossible(int x, int y, int a, int b) {
         x=abs(x);
         y=abs(y);
         a=abs(a);
         b=abs(b);
-        if(__gcd(x,y)==__gcd(a,b))return 1;
+        if(gcd(x,y)==gcd(a,b))return 1;
         return 0;
     }
 };
