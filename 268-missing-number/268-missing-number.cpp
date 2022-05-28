091762b1599545ecb027b3879519ed1e06060789
MridulGupta75 <66024896+MridulGupta75@users.noreply.github.com>
2022-05-28 16:20:23 +0530

    Time: 24 ms (63.03%), Space: 18 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        int z=0;
+       string z="positive";
         for(int i=0;i<n;i++)
         {
             if(abs(nums[i])==n)continue;
@@ -10,14 +10,14 @@ public:
             {
                 if( nums[abs(nums[i])]!=0)
                 nums[abs(nums[i])]*=-1;
-                else z=1;
+                else z="negative";
             }
         }
          for(int i=0;i<n;i++)
          {
             if(nums[i]>=0)
             {
-                if(nums[i]==0&&z==0)n=i;
+                if(nums[i]==0&&z=="positive")n=i;
                 else if(nums[i]!=0)n=i;
             }
          }
