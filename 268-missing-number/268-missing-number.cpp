97c9995f0ec4a31052473649759a0b9c464d7bf5
MridulGupta75 <66024896+MridulGupta75@users.noreply.github.com>
2022-05-28 16:35:21 +0530

    Time: 22 ms (72.29%), Space: 18 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
+    // int missingNumber(vector<int>& nums) {
+    //     int n=nums.size();
+    //    string z="positive";
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(abs(nums[i])==n)continue;
+    //         else
+    //         {
+    //             if( nums[abs(nums[i])]!=0)
+    //             nums[abs(nums[i])]*=-1;
+    //             else z="negative";
+    //         }
+    //     }
+    //      for(int i=0;i<n;i++)
+    //      {
+    //         if(nums[i]>=0)
+    //         if(nums[i]==0&&z=="positive"||(nums[i]!=0))n=i;
+    //      }
+    //     return n;
+    //}
+    
+    
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-       string z="positive";
+        int s=0;
         for(int i=0;i<n;i++)
-        {
-            if(abs(nums[i])==n)continue;
-            else
-            {
-                if( nums[abs(nums[i])]!=0)
-                nums[abs(nums[i])]*=-1;
-                else z="negative";
-            }
-        }
-         for(int i=0;i<n;i++)
-         {
-            if(nums[i]>=0)
-            if(nums[i]==0&&z=="positive"||(nums[i]!=0))n=i;
-         }
-        return n;
+            s+=nums[i];
+        n=n*(n+1)/2-s;
+       return n;
     }
+    
+    
+    
 };
