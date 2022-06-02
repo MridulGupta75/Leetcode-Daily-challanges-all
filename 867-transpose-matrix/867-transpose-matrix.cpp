acfcdec5bf9c5004090c69844f3727be018a997e
MridulGupta75 <66024896+MridulGupta75@users.noreply.github.com>
2022-06-02 10:36:43 +0530

    Time: 16 ms (54.24%), Space: 10.6 MB (78.16%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        vector<vector<int>>sol;
+        vector<vector<int>>sol(matrix[0].size(),vector<int>(matrix.size(),0));
         
-       for(int i=0;i<matrix[0].size();i++)
-        {
-           vector<int>sub;
-            for(int j=0;j<matrix.size();j++)
-            {
-                sub.push_back(0);
-            }
-           sol.push_back(sub);
-        }
+       // for(int i=0;i<matrix[0].size();i++)
+       //  {
+       //     vector<int>sub;
+       //      for(int j=0;j<matrix.size();j++)
+       //      {
+       //          sub.push_back(0);
+       //      }
+       //     sol.push_back(sub);
+       //  }
         for(int i=0;i<matrix.size();i++)
         {
             for(int j=0;j<matrix[0].size();j++)
