889c04486f4c9258a4893a7e7dfcb0d4745c194e
MridulGupta75 <66024896+MridulGupta75@users.noreply.github.com>
2022-06-20 13:40:45 +0530

    Time: 17 ms (81.40%), Space: 10.7 MB (64.93%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,13 +4,13 @@ public:
         int m=text1.length(),n=text2.length();
         int dp[m+1][n+1];
         for(int i=0;i<=m;i++)
-            dp[i][0]=0;
-        for(int i=0;i<=n;i++)
-            dp[0][i]=0;
-        for(int i=1;i<=m;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j=0;j<=n;j++)
             {
+                if(i==0||j==0)
+                    dp[i][j]=0;
+                else
+                {
                 
                     if(text1[i-1]==text2[j-1])
                         dp[i][j]=1+dp[i-1][j-1];
@@ -18,6 +18,7 @@ public:
                     {
                         dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
                     }
+                }
                 
             }
         }
