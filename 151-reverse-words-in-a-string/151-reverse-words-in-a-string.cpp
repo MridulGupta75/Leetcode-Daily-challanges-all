d93cc2d2148b55b748098fd05f92fde3b9964159
MridulGupta75 <66024896+MridulGupta75@users.noreply.github.com>
2022-09-29 09:41:33 +0530

    Time: 39 ms (7.74%), Space: 47.8 MB (5.28%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -2,22 +2,6 @@
 class Solution {
 public:
     string reverseWords(string s) {
-      //   string str="",str1="";
-      //   for(int i=0;i<s.length();i++)
-      //   {
-      //       if(s[i]==' ')
-      //       {
-      //           str=str1+' '+str;
-      //           str1="";
-      //       }
-      //       else
-      //       {
-      //           str1+=s[i];
-      //       }
-      //   }
-      //   str=str1+' '+str;
-      // str.erase(str.find_last_not_of(" \n\r\t")+1);
-      //   return str;
         stack<string>st;
         string str="";
         for(int i=0;i<s.length();i++)
@@ -27,8 +11,7 @@ public:
                 st.push(str);
                 str="";
             }
-            else
-                str+=s[i];
+            else str+=s[i];
         }
         st.push(str);
         string so="";
@@ -41,11 +24,8 @@ public:
                 else
                 so=so+' '+st.top();
             }
-           
-            
-            st.pop();
+           st.pop();
         }
-        
         return so;
    }
 };
