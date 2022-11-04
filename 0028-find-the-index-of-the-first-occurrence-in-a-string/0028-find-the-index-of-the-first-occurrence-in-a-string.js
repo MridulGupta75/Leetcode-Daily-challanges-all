9006c92452411dde029d5a6b782c60afe4cba784
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2022-11-04 19:40:51 +0530

    Time: 97 ms (52.00%), Space: 41.8 MB (74.89%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    var x=haystack.indexOf(needle);
+    return x;
+};
