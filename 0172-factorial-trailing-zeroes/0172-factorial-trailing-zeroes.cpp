77f469d61d3e58ab39487672c67f10d1c44e1772
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2022-11-15 20:51:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (5.11%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int sum=0,i=1;
+        int i=1,sum=0;
         while(floor(n/pow(5,i))>0)
         {
             sum+=floor(n/pow(5,i));
             i++;
-            
         }
         return sum;
     }
