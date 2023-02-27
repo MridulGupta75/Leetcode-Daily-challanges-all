8ae2b731303896a31d42367f49e2b301617a1a9d
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-02-27 19:27:51 +0530

    Time: 3 ms (72.73%), Space: 7.8 MB (24.34%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     
-    int fun(vector<int>&nums,int i,int j)
+    int fun(vector<int>&nums,int i,int j, vector<vector<int>>&dp)
     {
         if(i==j)return nums[j];
-        int choosefirst=nums[i]-fun(nums,i+1,j);
-         int chooseleft=nums[j]-fun(nums,i,j-1);
-        return max(choosefirst,chooseleft);
-        
+          if(dp[i][j]==-1){
+        int choosefirst=nums[i]-fun(nums,i+1,j,dp);
+         int chooseleft=nums[j]-fun(nums,i,j-1,dp);
+        dp[i][j]= max(choosefirst,chooseleft);
+          }
+        return dp[i][j];
     }
     bool PredictTheWinner(vector<int>& nums) {
         int i=0,j=nums.size()-1;
-        if(fun(nums,i,j)>=0)return true;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        if(fun(nums,i,j,dp)>=0)return true;
         return false;
         
     }
