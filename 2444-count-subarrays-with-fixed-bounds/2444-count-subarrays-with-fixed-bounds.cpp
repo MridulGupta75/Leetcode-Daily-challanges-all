6ebfe64d3757683f4166db687821f4f902efce21
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-03-05 01:53:01 +0530

    Time: 112 ms (86.67%), Space: 70.4 MB (90.00%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -5,17 +5,12 @@ public:
         long long sol=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]<mink||nums[i]>maxk){
-                li=i;
-                mik=-1;
-                mak=-1;
-            }
+            if(nums[i]<mink||nums[i]>maxk) li=i,mik=-1,mak=-1;
             else
             {
                 if(nums[i]==mink)mik=i;
                 if(nums[i]==maxk)mak=i;
-                if(mik!=-1&&mak!=-1)
-                    sol+=min(mik,mak)-li;
+                if(mik!=-1&&mak!=-1) sol+=min(mik,mak)-li;
             }
         }
         return sol;
