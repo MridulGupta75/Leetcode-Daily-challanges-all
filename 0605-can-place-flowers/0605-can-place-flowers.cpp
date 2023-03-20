edb0da389e7a1c4ee532f0421ed31833630f9c8e
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-03-20 20:52:08 +0530

    Time: 21 ms (48.13%), Space: 20.1 MB (99.96%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -5,20 +5,10 @@ public:
         
         for(int i=0;i<flowerbed.size();i++)
         {
-            cout<<i;
             if(flowerbed[i]==1)i++;
-            else
-            {
-                if((i==0||flowerbed[i-1]==0)&&(i==flowerbed.size()-1||flowerbed[i+1]==0))
-                {
-                    n--;
-                    flowerbed[i]=1;
-                }
-            }
-            
+            else 
+             if((i==0||flowerbed[i-1]==0)&&(i==flowerbed.size()-1||flowerbed[i+1]==0))n--,flowerbed[i]=1;
             if(n==0)return true;
-            
-            
         }
         return false;
     }
