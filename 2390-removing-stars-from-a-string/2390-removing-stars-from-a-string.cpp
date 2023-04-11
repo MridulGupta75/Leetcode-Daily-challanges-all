d73bd7bc31a17a79402fb85fddfb27e90d318823
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-04-11 21:05:02 +0530

    Time: 85 ms (67.67%), Space: 27.1 MB (28.68%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,63 @@
+// class Solution {
+// public:
+//     string removeStars(string s) {
+//         string ans="";
+//         stack<char>st;
+//         for(auto it:s)
+//         { 
+//           if(it=='*' && !st.empty())
+//           {
+//               st.pop();
+//           }
+//             else
+//             {
+//                 st.push(it);
+//             }
+            
+//         }
+        
+//         while(!st.empty())
+//         {
+//             ans=st.top()+ans;
+//             // cout<<st.top();
+//             st.pop();
+//         }
+//         return ans;
+        
+//     }
+// };
+
+
+
+class Solution {
+public:
+    string removeStars(string s) {
+        // create a stack to store the characters
+        stack<char> st;
+        
+        // iterate through each character in the input string
+        for (char c : s) {
+            // if the current character is a star and the stack is not empty, pop the topmost character
+            // from the stack
+            if (c == '*' && !st.empty()) {
+                st.pop();
+            } else {
+                // otherwise, push the current character onto the stack
+                st.push(c);
+            }
+        }
+        
+        // create a string to store the result
+        string ans;
+        
+        // pop the elements of the stack and add them to the result string
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        
+        // reverse the result string and return it as the output
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
