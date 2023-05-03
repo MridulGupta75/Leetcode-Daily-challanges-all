f5c38a04d656a76be716944ac18310426761d654
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-03 22:53:14 +0530

    Time: 55 ms (76.30%), Space: 31.6 MB (68.55%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -5,12 +5,8 @@ public:
    unordered_set<int>s1(nums2.begin(),nums2.end());
         vector<int>v1;
         vector<int>v2;
-        vector<vector<int>>sol;
         for(int num:s) if(s1.count(num)==0) v1.push_back(num);
         for(int num:s1) if(s.count(num)==0) v2.push_back(num);
-        
-        sol.push_back(v1);
-        sol.push_back(v2);
-        return sol;
+         return {v1,v2};
     }
 };
