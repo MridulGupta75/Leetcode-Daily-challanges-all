602acac88847c788508f28d64ee762e8cadc909d
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-10 16:57:05 +0530

    Time: 30 ms (5.04%), Space: 13.3 MB (73.36%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int,int>>x;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==0)
+                    x.push_back({i,j});
+            }
+        }
+        for(auto k:x)
+        {
+            cout<<k.first<<" "<<k.second<<endl;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(i==k.first||j==k.second)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        }
+        
+        
+    }
+};
