db2ba917c2218029c85f26409cc956e50515036f
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-15 15:32:44 +0530

    Time: 3 ms (62.93%), Space: 8.9 MB (89.30%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,12 +4,8 @@ public:
         int i=m-1,j=n-1,k=m+n-1;
         while(j>=0)
         {
-            if(i>=0&&nums1[i]>nums2[j])
-            {
-                nums1[k--]=nums1[i--];
-            }
-            else
-                nums1[k--]=nums2[j--];
+            if(i>=0&&nums1[i]>nums2[j]) nums1[k--]=nums1[i--];
+            else nums1[k--]=nums2[j--];
         }
         
     }
