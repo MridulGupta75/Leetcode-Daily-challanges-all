7da364c02d970a27a8f78f9f6690e048413f8355
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-18 02:20:32 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
+    double myPow(double x, int k) {
         if(x==1)return 1;
-        else if(x==0)return 0;
-        if(n==0)return 1;
-        long long int k=n;
+        if(x==0)return 0;
+        if(k==0)return 1;
+        if(k==1)return x;
+       double res=1;
         bool flag=false;
-        double res=1;
-        if(n<0){
-            flag=true;
-            k=k*-1;
-        }
-        while(k>0)
+        long long int n=k;
+        if(n<0)
         {
-            if(k%2==1)
+            flag=true;
+            n*=-1;
+        }
+        while(n>0)
+        {
+            if(n%2==0)
             {
-             res=res*x;
-            k--;
+                x=x*x;
+                n=n/2;
             }
-          else{
-              x=x*x;
-              k=k/2;
-          }
-        } 
-        
-       if(flag==true)return (1/res);
+            else
+            {
+                res=res*x;
+                n--;
+            }
+        }
+        if(flag==true)return(1/res);
         return res;
     }
 };
