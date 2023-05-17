ad9666cdc7a26faee43d772d52a4f2b9ed6459b3
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-18 02:42:56 +0530

    Time: 23 ms (46.59%), Space: 19.6 MB (72.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,26 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int>mpp;
-        for(auto x:nums)
-        {
-            auto itr=mpp.find(x);
-            if(itr==mpp.end())
-            {
-                mpp.insert({x,1});
-            }
-            else itr->second=(itr->second)+1;
-        }
-        int max=INT_MIN;
-        int pos;
-        for(auto x:mpp)
-        {
-            if(x.second>max)
-            {
-            max=x.second;
-             pos=x.first;
-            }
-        }
-        return pos;
+        sort(nums.begin(),nums.end());
+        return(nums[nums.size()/2]);
     }
 };
