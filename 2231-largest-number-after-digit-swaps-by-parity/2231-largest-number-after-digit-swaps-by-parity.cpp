cc766dfdab243fc88714d1d50c6ba71310d74600
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-19 00:30:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (69.42%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int largestInteger(int num) {
+        priority_queue<int>odd;
+        priority_queue<int>e;
+        string n=to_string(num);
+        for(auto&x:n)
+        {
+            if((x-'0')%2)
+                odd.push(x-'0');
+            else
+                e.push(x-'0');
+        }
+        int ans=0;
+    for(auto&x:n)
+    {
+        ans*=10;
+        if((x-'0')%2)
+        {
+            
+          ans+=(odd.top());
+            odd.pop();
+        }
+        else
+        {
+            ans+=(e.top());
+            e.pop();
+        }
+    }
+        return ans;
+    }
+};
