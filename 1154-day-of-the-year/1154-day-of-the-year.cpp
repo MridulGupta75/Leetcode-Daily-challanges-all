5a744bbcd75418dce3f2e2fd8a9fb218bf21490b
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-19 01:09:32 +0530

    Time: 25 ms (52.05%), Space: 6.8 MB (39.49%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dayOfYear(string date) {
+        vector<int>mth{31,28,31,30,31,30,31,31,30,31,30,31};
+      int y=stoi(date.substr(0,4));
+        int m=stoi(date.substr(5,7));
+        int d=stoi(date.substr(8));
+       int ans=0;
+        int i=0;
+        int m1=m;
+        m--;
+        while(m--)
+        {
+            ans+=mth[i++];
+        }
+        ans+=d;
+        if(y%4==0&&y%100!=0||y%400==0)
+        {
+            cout<<"sdgh";
+            if(m1>2)return ans+1;
+        }
+        return ans;
+    }
+};
