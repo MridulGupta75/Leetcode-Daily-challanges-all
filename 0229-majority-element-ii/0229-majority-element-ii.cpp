4e1bed685350700aeaa8422392f8d1b629278633
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-18 19:23:56 +0530

    Time: 19 ms (25.04%), Space: 15.8 MB (96.28%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int el1=INT_MIN,el2=INT_MIN;
         vector<int>sol;
-        unordered_map<int,int>mpp;
+       int ct1=0,ct2=0;
         for(auto &x:nums)
         {
-            auto itr=mpp.find(x);
-            if(itr!=mpp.end())
+            if(ct1==0&&el2!=x)
             {
-                itr->second=(itr->second)+1;
+                el1=x;
+                ct1=1;
             }
-            else{
-                mpp.insert({x,1});
+            else if(ct2==0&&el1!=x)
+            {
+                el2=x;
+                ct2=1;
+            }
+            else if(x==el1)
+            {
+                ct1++;
+            }
+            else if(x==el2)
+            {
+                ct2++;
+            }
+            else
+            {
+                ct1--;
+                ct2--;
             }
         }
-        int n=(nums.size())/3;
-        for(auto &x:mpp)
+        int n=nums.size()/3;
+        int ct11=0,ct22=0;
+        for(auto &x:nums)
         {
-            if(x.second>n)sol.push_back(x.first);
+            if(x==el1)ct11++;
+            if(x==el2)ct22++;
         }
+        if(ct11>n)sol.push_back(el1);
+        if(ct22>n)sol.push_back(el2);
         return sol;
-        }
+    }
 };
