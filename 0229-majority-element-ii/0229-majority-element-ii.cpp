3500d08b33293e3b2167f253427b8616d4f4cf6e
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-18 14:34:01 +0530

    Time: 19 ms (25.04%), Space: 15.9 MB (51.22%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int>sol;
+        map<int,int>mpp;
+        for(auto x:nums)
+        {
+            auto itr=mpp.find(x);
+            if(itr!=mpp.end())
+            {
+                itr->second=(itr->second)+1;
+            }
+            else{
+                mpp.insert({x,1});
+            }
+        }
+        int n=(nums.size())/3;
+        for(auto x:mpp)
+        {
+            if(x.second>n)sol.push_back(x.first);
+        }
+        return sol;
+        }
+};
