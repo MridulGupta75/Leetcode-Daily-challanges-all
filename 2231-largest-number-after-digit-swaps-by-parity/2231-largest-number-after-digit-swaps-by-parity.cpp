353557f65ee635b728c68c986d30fe4ee0d4249d
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-19 12:33:43 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.74%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     int largestInteger(int num) {
-        priority_queue<int>odd;
-        priority_queue<int>e;
+        vector<int>odd;
+        vector<int>e;
         string n=to_string(num);
         for(auto&x:n)
         {
             if((x-'0')%2)
-                odd.push(x-'0');
+                odd.push_back(x-'0');
             else
-                e.push(x-'0');
+                e.push_back(x-'0');
         }
+        sort(odd.begin(),odd.end(),greater<int>());
+        sort(e.begin(),e.end(),greater<int>());
         int ans=0;
+        int i=0;
+        int j=0;
     for(auto&x:n)
     {
         ans*=10;
         if((x-'0')%2)
         {
-           ans+=(odd.top());
-            odd.pop();
+           ans+=(odd[i++]);
+            
         }
         else
         {
-            ans+=(e.top());
-            e.pop();
+            ans+=(e[j++]);
+           
         }
     }
         return ans;
