fbb7b3f4b2e671171a97e341f3111054b36cdfaf
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-19 19:08:50 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (76.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int N=m+n-1;
-        int r=min(m,n)-1;
+        int r=min(n,m)-1;
         double res=1;
         for(int i=1;i<=r;i++)
         res=res*(N-i)/i;
-      
-        return (int)res;
+        return res;
     }
 };
