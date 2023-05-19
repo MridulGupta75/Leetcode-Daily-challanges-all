93f933f77412584e3e58a688dd082e29365ebdf2
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-19 15:35:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (54.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>sol(m,vector<int>(n,-1));
         int i=0,j=0;
-        return (count(i,j,m,n,sol));
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return(count(i,j,m,n,dp));
+        
+        
     }
     int count(int i,int j,int m,int n,vector<vector<int>>&dp)
     {
-         if(n<=j||m<=i) return 0;
         if(i==(m-1)&&j==(n-1))dp[i][j]=1;
-       
-        if(dp[i][j]!=-1)return dp[i][j];
+         if(i>=m||j>=n)return 0;
+        if(dp[i][j]==-1)
         return(dp[i][j]=count(i+1,j,m,n,dp)+count(i,j+1,m,n,dp));
+        return dp[i][j];
+        
     }
 };
