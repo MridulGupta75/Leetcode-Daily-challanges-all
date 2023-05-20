8233f9ce38beed2891b02bc949146eaab822b354
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-20 15:35:48 +0530

    Time: 56 ms (6.05%), Space: 71.4 MB (80.06%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -16,9 +16,15 @@ public:
         ListNode*sol=NULL;
         ListNode*temp=NULL;
         int c=0;
-        while(l1!=NULL&&l2!=NULL)
+        while(l1!=NULL||l2!=NULL)
         {
-            int sum=l1->val+l2->val+c;
+            int sum;
+           if(l1!=NULL&&l2!=NULL)
+                sum=l1->val+l2->val+c;
+               else if(l2==NULL&&l1!=NULL)
+                 sum=l1->val+c;
+                else
+                    sum=l2->val+c;
             cout<<sum<<" ";
             ListNode*t=new ListNode();
              if(sol==NULL)
@@ -42,68 +48,70 @@ public:
                     temp->next=t;
                     temp=t;
                 }
+            if(l1!=NULL)
             l1=l1->next;
+            if(l2!=NULL)
             l2=l2->next;
         }
         
-        while(l1!=NULL)
-        {
-            int sum=l1->val+c;
+//         while(l1!=NULL)
+//         {
+//             int sum=l1->val+c;
            
-            ListNode*t=new ListNode();
-             if(sol==NULL)
-                 sol=t;
-            if(sum>9)
-            {
-                t->val=sum%10;
-                c=sum/10;
-            }
-            else
-            {
-                t->val=sum;
-             c=0;
-            }
-             if(sol==t)
-                {
-                    temp=t;
-                }
-                else
-                {
-                    temp->next=t;
-                    temp=t;
-                }
-            l1=l1->next;
-        }
-         while(l2!=NULL)
-        {
-            int sum=l2->val+c;
+//             ListNode*t=new ListNode();
+//              if(sol==NULL)
+//                  sol=t;
+//             if(sum>9)
+//             {
+//                 t->val=sum%10;
+//                 c=sum/10;
+//             }
+//             else
+//             {
+//                 t->val=sum;
+//              c=0;
+//             }
+//              if(sol==t)
+//                 {
+//                     temp=t;
+//                 }
+//                 else
+//                 {
+//                     temp->next=t;
+//                     temp=t;
+//                 }
+//             l1=l1->next;
+//         }
+//          while(l2!=NULL)
+//         {
+//             int sum=l2->val+c;
            
-            ListNode*t=new ListNode();
-             if(sol==NULL)
-                 sol=t;
-            if(sum>9)
-            {
+//             ListNode*t=new ListNode();
+//              if(sol==NULL)
+//                  sol=t;
+//             if(sum>9)
+//             {
                 
-                t->val=sum%10;
+//                 t->val=sum%10;
              
-                c=sum/10;
-            }
-            else
-            {
-                t->val=sum;
-               c=0;
-            }
-             if(sol==t)
-                {
-                    temp=t;
-                }
-                else
-                {
-                    temp->next=t;
-                    temp=t;
-                }
-            l2=l2->next;
-        }
+//                 c=sum/10;
+//             }
+//             else
+//             {
+//                 t->val=sum;
+//                c=0;
+//             }
+//              if(sol==t)
+//                 {
+//                     temp=t;
+//                 }
+//                 else
+//                 {
+//                     temp->next=t;
+//                     temp=t;
+//                 }
+//             l2=l2->next;
+//         }
         if(c!=0)
         {
             ListNode*t=new ListNode();
