4c5c38e29ec9b33371de4bf8c82ad97e84253aed
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-21 01:02:17 +0530

    Time: 131 ms (40.26%), Space: 13.2 MB (64.82%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {\
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
          vector<vector<int>>sol;
         if(nums.size()<4)return sol;
-        set<vector<int>>s;
+        // set<vector<int>>s;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-3;i++)
         {
+            if(i>0&&nums[i]==nums[i-1])continue;
            for(int j=i+1;j<nums.size()-2;j++)
            {
+               if(j>i+1&&nums[j]==nums[j-1])continue;
                int l=j+1,h=nums.size()-1;
                while(l<h)
                {
@@ -16,7 +19,9 @@ public:
                    
                    if((long long)nums[l]+(long long)nums[h]==target-(long long)nums[i]-(long long)nums[j])
                    {
-                       s.insert({nums[i],nums[j],nums[l],nums[h]});
+                       sol.push_back({nums[i],nums[j],nums[l],nums[h]});
+                       while(l<h&&nums[l+1]==nums[l])l++;
+                       while(l<h&&nums[h-1]==nums[h])h--;
                        l++,h--;
                    }
                    else if((long long)nums[l]+(long long)nums[h]<target-(long long)nums[i]-(long long)nums[j])l++;
@@ -26,8 +31,8 @@ public:
             
         }
        
-        for(auto&x:s)
-            sol.push_back(x);
+        // for(auto&x:s)
+            // sol.push_back(x);
         return sol;
         
     }
