ce9e3af31045dbcc564a9ffddf7697f1d6747b24
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-20 21:38:04 +0530

    Time: 242 ms (47.21%), Space: 24 MB (91.05%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>>s;
+        // set<vector<int>>s;
+         vector<vector<int>>sol;
          sort(nums.begin(),nums.end());                                       
         for(int i=0;i<nums.size()-2;i++)
         {
+            if(i==0||(i>0&&nums[i]!=nums[i-1]))
+            {
             int l=i+1;
             int h=nums.size()-1;
             while(l<h)
             {
             if(nums[i]+nums[l]+nums[h]==0)
             {
-                s.insert({nums[i],nums[l],nums[h]});  
+                sol.push_back({nums[i],nums[l],nums[h]}); 
+                while(l<h&&nums[l]==nums[l+1])l++;
+                 while(l<h&&nums[h]==nums[h-1])h--;
                 l++,h--;
             }
-            else if(nums[l]+nums[h]<-1*nums[i])
-            {
-                l++;
-            }
+            else if(nums[l]+nums[h]<-1*nums[i]) l++;
             else h--;
             }
+            }
         }
-        vector<vector<int>>sol;
-        for(auto&x:s) sol.push_back(x);
+       
+        // for(auto&x:s) sol.push_back(x);
             return sol;
         
     }
