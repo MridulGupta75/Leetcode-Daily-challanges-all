38d313bdf94de99955358de8bec29cdc13fbf5b2
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-21 19:12:06 +0530

    Time: 5 ms (32.28%), Space: 7.2 MB (98.50%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minStartValue(vector<int>& nums) {
         bool flag=false;
-        if(nums[0]<0)flag=true;
+        // if(nums[0]<0)flag=true;
         for(int i=1;i<nums.size();i++)
         {
            
@@ -14,8 +14,9 @@ public:
         for(auto&x:nums)
             if(x<min)
                 min=x;
-        if(!flag)return 1;
+      if(min<0)
         return abs(min)+1;
+        else return 1;
       
             
     }
