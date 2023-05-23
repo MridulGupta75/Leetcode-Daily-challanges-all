90fc48855fae65572fbeb02cc7697a76742b7bb5
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-23 22:26:19 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -13,11 +13,11 @@ class Solution
     int func(int ind1,int ind2,string s1, string s2,vector<vector<int>>&dp)
     {
         //base case
-        if(ind1<0||ind2<0)return 0;
+        if(ind1==0||ind2==0)return 0;
         //main case
         //if satisfy
         if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(s1[ind1]==s2[ind2])
+        if(s1[ind1-1]==s2[ind2-1])
         return dp[ind1][ind2]=1+func(ind1-1,ind2-1,s1,s2,dp);
         //if not 
         //check take 
@@ -29,8 +29,8 @@ class Solution
     
     int lcs(int x, int y, string s1, string s2)
     {
-        vector<vector<int>>dp(x,vector<int>(y,-1));
-          return func(x-1,y-1,s1,s2,dp);
+        vector<vector<int>>dp(x+1,vector<int>(y+1,-1));
+          return func(x,y,s1,s2,dp);
     }
 };
 
