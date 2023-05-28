4c0bb2c1083f9aada30643efac009654c6414fc3
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-28 15:25:03 +0530

    Time: 6 ms (73.60%), Space: 7 MB (99.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -4,10 +4,21 @@ class Solution {
     
     bool check(int n,int i,int col,vector<string>&subsol)
     {
+        //jaha place karni h whaa se bas 3 direction check karo 
+       
+        
+        /*
+        . . . .    
+        . . . .      .   
+        . . . .      .   (.Q)->for this check in these direction
+        . . . .      .
+        
+        */
+        
         
         int cs=col;
         int is=i;
-        
+         //bottom diagonal
         while(i>=0&&col>=0)
         {
             if(subsol[i][col]=='Q')return false;
@@ -16,6 +27,9 @@ class Solution {
         }
         i=is;
         col=cs;
+        
+        
+        //sidha
          while(col>=0)
         {
             if(subsol[i][col]=='Q')return false;
@@ -25,6 +39,9 @@ class Solution {
         
          i=is;
         col=cs;
+        //top diagonal
+        
+        
          while(col>=0&&i<n)
         {
             if(subsol[i][col]=='Q')return false;
@@ -37,18 +54,25 @@ class Solution {
     
     void func(int col,vector<vector<string>>&sol,vector<string>&subsol,int n)
     {
+        //agr col bhar aata h tw wo soln h daldo
         if(col==n)
         {
             sol.push_back(subsol);
             return ;
         }
+        
+        
+        //sara row ka lia chalooo
         for(int i=0;i<n;i++)
         {
+            //check karo ki wah lag sakta h ya ni
             if(check(n,i,col,subsol))
             {
+                //queen dalo
                 subsol[i][col]='Q';
                 func(col+1,sol,subsol,n);
-                 subsol[i][col]='.';
+            //backtrack marao hata ka q ko
+                subsol[i][col]='.';
             }
         }
     }
