38fa510d550279238817e22ad4636a411e295f78
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-28 15:16:10 +0530

    Time: 8 ms (59.52%), Space: 7 MB (99.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,9 +1,13 @@
 class Solution {
     
-    bool check(int i,int col,vector<string>subsol,int n)
+    
+    
+    bool check(int n,int i,int col,vector<string>&subsol)
     {
+        
         int cs=col;
         int is=i;
+        
         while(i>=0&&col>=0)
         {
             if(subsol[i][col]=='Q')return false;
@@ -12,55 +16,54 @@ class Solution {
         }
         i=is;
         col=cs;
-        while(col>=0)
+         while(col>=0)
         {
             if(subsol[i][col]=='Q')return false;
             col--;
-   
         }
-        i=is;
+        
+        
+         i=is;
         col=cs;
-        while(col>=0&&i<n)
+         while(col>=0&&i<n)
         {
             if(subsol[i][col]=='Q')return false;
-            i++;
             col--;
-   
+             i++;
         }
         return true;
-        
     }
     
-    void func(vector<vector<string >>&sol,vector<string>&subsol,int col,int n)
+    
+    void func(int col,vector<vector<string>>&sol,vector<string>&subsol,int n)
     {
         if(col==n)
         {
             sol.push_back(subsol);
-            return;
+            return ;
         }
         for(int i=0;i<n;i++)
         {
-            if(check(i,col,subsol,n))
+            if(check(n,i,col,subsol))
             {
                 subsol[i][col]='Q';
-                    func(sol,subsol,col+1,n);
+                func(col+1,sol,subsol,n);
                  subsol[i][col]='.';
             }
         }
-        
     }
-    
     
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string >>sol;
+        
+        vector<vector<string>>sol;
         vector<string>subsol;
         string s=string(n,'.');
         for(int i=0;i<n;i++)
-        subsol.push_back(s);
-        
-        func(sol,subsol,0, n);
+            subsol.push_back(s);
+        func(0,sol,subsol,n);
         return sol;
-
+        
+        
     }
 };
