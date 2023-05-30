0ae62c360534ebcfc4e519f4c58187ba2a7d78e6
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-31 00:39:26 +0530

    Time: 15 ms (92.09%), Space: 19.5 MB (66.40%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    //moores voting algo
     int majorityElement(vector<int>& nums) {
-        int el=nums[0];
         int ct=1;
+        int ele=nums[0];
         for(int i=1;i<nums.size();i++)
         {
             if(ct==0)
             {
-                el=nums[i];
                 ct=1;
+                ele=nums[i];
+                
             }
-            else if(el==nums[i])ct++;
+            else if(ele==nums[i])ct++;
             else ct--;
         }
         int ct1=0;
         for(auto&x:nums)
-        {
-            if(x==el)ct1++;
-        }
-        if(ct1>nums.size()/2)return el;
-         return -1;
+            if(ele==x)ct1++;
+        return((ct1>nums.size()/2?ele:-1));
     }
 };
