70f9a61e20fa908c451c6fc9a28cb0468dd66ba3
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-31 00:53:37 +0530

    Time: 8 ms (93.29%), Space: 15.9 MB (75.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int el1=INT_MIN,el2=INT_MIN;
         vector<int>sol;
-       int ct1=0,ct2=0;
-        for(auto &x:nums)
+        int s1=INT_MIN,s2=INT_MIN;
+        int ct1=0, ct2=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(ct1==0&&el2!=x)
+            if(ct1==0&&s2!=nums[i])
             {
-                el1=x;
                 ct1=1;
+                s1=nums[i];
             }
-            else if(ct2==0&&el1!=x)
+             else if(ct2==0&&s1!=nums[i])
             {
-                el2=x;
                 ct2=1;
+                s2=nums[i];
             }
-            else if(x==el1)
-            {
-                ct1++;
-            }
-            else if(x==el2)
-            {
-                ct2++;
-            }
-            else
-            {
-                ct1--;
-                ct2--;
-            }
+            else if(s1==nums[i])ct1++;
+            else if(s2==nums[i])ct2++;
+            else ct1--,ct2--;
         }
-        int n=nums.size()/3;
         int ct11=0,ct22=0;
-        for(auto &x:nums)
+        for(auto&x:nums)
         {
-            if(x==el1)ct11++;
-            if(x==el2)ct22++;
+            if(x==s1)ct11++;
+            if(x==s2)ct22++;
         }
-        if(ct11>n)sol.push_back(el1);
-        if(ct22>n)sol.push_back(el2);
+     
+        if(ct11>nums.size()/3)sol.push_back(s1);
+         if(ct22>nums.size()/3)sol.push_back(s2);
         return sol;
+        
     }
 };
