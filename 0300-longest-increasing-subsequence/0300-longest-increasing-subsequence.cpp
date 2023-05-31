ec1d1e5bed3969f4fe92c7f5baf6d1189bd80c12
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-05-31 13:13:13 +0530

    Time: 447 ms (39.25%), Space: 10.5 MB (68.44%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,15 +4,9 @@ public:
         
         vector<int>dp(nums.size(),1);
         for(int i=1;i<nums.size();i++)
-        {
-            for(int j=0;j<i;j++)
-            {
+        for(int j=0;j<i;j++)
                 if(nums[i]>nums[j])
-                {
-                    dp[i]=max(dp[i],dp[j]+1);
-                }
-            }
-        }
+                dp[i]=max(dp[i],dp[j]+1);
         return(*max_element(dp.begin(),dp.end()));
         
     }
