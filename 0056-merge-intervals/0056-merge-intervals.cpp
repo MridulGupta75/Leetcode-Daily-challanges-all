c29685849591371d27781791da208d8956e9dae4
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-01 00:18:50 +0530

    Time: 45 ms (51.07%), Space: 18.8 MB (94.32%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,38 +1,38 @@
-// class Solution {
-// public:
-//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-//         vector<vector<int>>v;
-//         v.push_back({intervals[0][0],intervals[0][1]});
-//         for(int i=1;i<intervals.size();i++)
-//         {
-//             if(v.back()[1]<intervals[i][0])
-//             {
-//                  v.push_back({intervals[i][0],intervals[i][1]});
-               
-//                 // v.back()[0]=min(intervals[i][0],v.back()[0]);
-//             }
-//             else
-//                   v.back()[1]=max(intervals[i][1],v.back()[1]);
-                 
-//         }
-//         return v;
-//     }
-// };
-
-
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>sol;
+        vector<vector<int>>v;
+        
         sort(intervals.begin(),intervals.end());
-        for(auto&x:intervals)
+        for(int i=0;i<intervals.size();i++)
         {
-            if(sol.empty()||sol.back()[1]<x[0])
-                sol.push_back(x);
+            if(v.empty()||v.back()[1]<intervals[i][0])
+            {
+                 v.push_back({intervals[i][0],intervals[i][1]});
+               
+                // v.back()[0]=min(intervals[i][0],v.back()[0]);
+            }
             else
-            sol.back()[1]=max(sol.back()[1],x[1]);
+                  v.back()[1]=max(intervals[i][1],v.back()[1]);
+                 
         }
-        return sol;
+        return v;
     }
-    
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         vector<vector<int>>sol;
+//         sort(intervals.begin(),intervals.end());
+//         for(auto&x:intervals)
+//         {
+//             if(sol.empty()||sol.back()[1]<x[0])
+//                 sol.push_back(x);
+//             else
+//             sol.back()[1]=max(sol.back()[1],x[1]);
+//         }
+//         return sol;
+//     }
+    
+// };
