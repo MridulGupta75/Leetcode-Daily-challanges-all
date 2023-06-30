24e9f75e28c9a0767dae0b616fa99fed59a76ebf
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-01 00:14:53 +0530

    Time: 45 ms (51.07%), Space: 18.9 MB (94.32%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,3 +1,25 @@
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         vector<vector<int>>v;
+//         v.push_back({intervals[0][0],intervals[0][1]});
+//         for(int i=1;i<intervals.size();i++)
+//         {
+//             if(v.back()[1]<intervals[i][0])
+//             {
+//                  v.push_back({intervals[i][0],intervals[i][1]});
+               
+//                 // v.back()[0]=min(intervals[i][0],v.back()[0]);
+//             }
+//             else
+//                   v.back()[1]=max(intervals[i][1],v.back()[1]);
+                 
+//         }
+//         return v;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
