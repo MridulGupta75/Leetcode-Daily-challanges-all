b205010280756bf2b6263dca75bc90f2f978cda2
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-03 18:07:38 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (85.37%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.length()!=goal.length())return false;
+        int pos1=-1,pos2=-1;
+         for(int i=0;i<s.length();i++)
+         {
+             if(s[i]!=goal[i]&&(pos1==-1||pos2==-1))
+             {
+                 if(pos1==-1)
+                     pos1=i;
+                 else
+                     pos2=i;
+             }
+             else if(s[i]!=goal[i]&&(pos1!=-1||pos2!=-1))
+                 return false;
+         }
+        if(pos1!=-1&&pos2==-1)return false;
+        if(pos1==-1)
+        {
+            map<int,int>mpp;
+             for(int i=0;i<s.length();i++)
+             {
+                 if(mpp.find(s[i])!=mpp.end())
+                     return true;
+                 else
+                     mpp.insert({s[i],1});
+             }
+            return false;
+        }
+        else
+        {
+            if(s[pos1]==goal[pos2]&&s[pos2]==goal[pos1]) return true;
+        }
+        return false;
+        
+    }
+};
