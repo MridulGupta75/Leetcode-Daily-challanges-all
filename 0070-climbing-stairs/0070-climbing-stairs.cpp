0977e7ad3d35f9e1982f15664a28b73f3e9c1096
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-05 02:43:04 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (14.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,11 +3,10 @@ public:
     
     int func(int n,vector<int>&dp)
     {
-        if(n==0)return 1;
+        if(n==0||n==1)return 1;
         if(dp[n-1]!=-1)return dp[n-1];
         int x=0,y=0;
          x=func(n-1,dp);
-        if(n>=2)
         y=func(n-2,dp);
         dp[n-1]=x+y;
         return dp[n-1];
