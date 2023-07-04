e357d80fa1dfd0e618ee16b35c4a0f81fa74f834
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-04 16:58:22 +0530

    Time: 3 ms (98.52%), Space: 9.5 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mpp;
+        int one =0,two=0;
         for(auto&x:nums)
         {
-            auto itr=mpp.find(x);
-            if(itr!=mpp.end())
-           itr->second=(itr->second)+1;
-            else
-                mpp.insert({x,1});
+            one=(one^x)&(~two);
+            two=(two^x)&(~one);
         }
-        for(auto&x:mpp)
-        {
-            cout<<x.first<<" "<<x.second<<endl;
-            if(x.second==1)
-                return x.first;
-        }
-        return 0;
+        return one;
     }
 };
