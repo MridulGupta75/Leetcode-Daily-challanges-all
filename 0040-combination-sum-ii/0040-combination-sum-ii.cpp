0b2e6f6fbc12043705427a4c623d93cc48de5552
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-04 23:57:51 +0530

    Time: 3 ms (90.53%), Space: 10.6 MB (80.29%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-     
-    void func(vector<vector<int>>&sol, vector<int>&subsol,vector<int>&nums,int target,int index)
+    void makeSum(vector<int>& nums, int target,vector<int>&subsol,vector<vector<int>>&sol,int index)
     {
-        //base
         if(target==0)
         {
             sol.push_back(subsol);
             return;
         }
-       
-        //condditions
-        //take
-        for(int i=index;i<nums.size();i++)
-        {
-            if(i>index&&nums[i]==nums[i-1])continue;
-            if(nums[i]>target)break;
-        subsol.push_back(nums[i]);
-        func(sol,subsol,nums,target-nums[i],i+1);
-        subsol.pop_back();
-        }
-
+      for(int i=index;i<nums.size();i++)
+      {
+          if(index!=i&&nums[i]==nums[i-1])continue;
+          if(target-nums[i]<0)break;
+          subsol.push_back(nums[i]);
+          makeSum(nums,target-nums[i],subsol,sol,i+1);
+          subsol.pop_back();
+      }
     }
-    
-    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         
-        
-        sort(candidates.begin(),candidates.end()); 
-        
-        vector<vector<int>>sol;
         vector<int>subsol;
-        
-        //call recursion function
-        func(sol,subsol,candidates,target,0);
-        
+        vector<vector<int>>sol;
+        sort(candidates.begin(),candidates.end());
+        makeSum(candidates,target,subsol,sol,0);
         return sol;
-        
     }
 };
