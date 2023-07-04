d9a91125a979958efec0018650b1ed5e5f17fb3a
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-05 02:14:27 +0530

    Time: 1983 ms (5.01%), Space: 292.2 MB (15.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,21 @@
 class Solution {
-    
-    int func(int index,int pre,vector<int>& nums,vector<vector<int>>&dp)
-    {
-        //base condition
-        if(index==nums.size())
-        {
-            return 0;
-        }
-        //conditions
-        int l=INT_MIN;
-        if(dp[index][pre+1]!=-1)return dp[index][pre+1];
-        if(pre==-1||nums[index]>nums[pre])
-        {
-            //take
-             l=1+func(index+1,index,nums,dp);
-           
-           
-        }
-         //not take
-         dp[index][pre+1]=max(l,0+func(index+1,pre,nums,dp));
-        return dp[index][pre+1];
-    }
-    
-    
-    
 public:
+    int LIS(vector<int>&nums,int ind,int pre,vector<vector<int>>&dp)
+    {
+        if(ind==nums.size())
+         return 0; 
+         //take
+        int l=INT_MIN;
+        if(dp[ind][pre+1]!=-1)return dp[ind][pre+1];
+        if(pre==-1||(nums[ind]>nums[pre]))
+        l=1+LIS(nums,ind+1,ind,dp);
+        //not take
+     dp[ind][pre+1]=max(LIS(nums,ind+1,pre,dp),l);
+        return dp[ind][pre+1];
+       
+    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
-        
-        return func(0,-1,nums,dp);
+       vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
+        return LIS(nums,0,-1,dp);
     }
 };
