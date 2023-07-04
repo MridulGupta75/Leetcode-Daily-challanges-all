c8a19ffac60f01323171ede681dd026fbb3f233c
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-04 17:31:19 +0530

    Time: 18 ms (80.54%), Space: 16.7 MB (98.77%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,9 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
         int one=0;
         for(auto&x:nums)
-        {
-            one=(one^x)&(~0);
-        }
+        one=(one^x)&(~0);
+        
         return one;
     }
 };
