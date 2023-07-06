138efbdd94de2a90744b05207ab52c60c3ffb805
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-06 23:52:22 +0530

    Time: 11 ms (64.37%), Space: 10 MB (44.62%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,27 +1,18 @@
-
-
-
-
 class Solution {
 public:
-    int m, n;
-    int solve(vector<vector<int>>&grid, int currRow, int currCol,vector<vector<int>>&memo)
+    int func(int i,int j,int m,int n,vector<vector<int>>& grid,vector<vector<int>>&dp)
     {
-        if (currRow >= m || currCol >= n) return INT_MAX; //when cell is out of bounds
-        if (currRow == m - 1 && currCol == n - 1) return grid[m - 1][n - 1]; //reached the target
-        if(memo[currRow][currCol]!=-1)return memo[currRow][currCol];
-        
-        int rightMin = solve(grid, currRow, currCol + 1,memo);
-        int downMin = solve(grid, currRow + 1, currCol,memo);
-        int bestChoice = min(rightMin, downMin);
-        
-        
-        return memo[currRow][currCol]=(grid[currRow][currCol] + bestChoice);
+        if(i<0||j<0)return 300;
+        if(i==0&&j==0)return grid[0][0];
+        if(dp[i][j]!=-1)return dp[i][j];
+        int top=func(i-1,j,m,n,grid,dp)+grid[i][j];
+        int down=func(i,j-1,m,n,grid,dp)+grid[i][j];
+        return(dp[i][j]=min(top,down));
     }
-    int minPathSum(vector<vector<int>>& grid) 
-    {
-        m = grid.size(); n = grid[0].size();
-        vector<vector<int>>memo (m,vector<int>(n,-1));
-        return solve(grid, 0, 0,memo);
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return(func(m-1,n-1,m,n,grid,dp));
     }
 };
