116986fac0d802a7edbc70a49178c1daa1722db5
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-06 22:17:15 +0530

    Time: 4 ms (55.99%), Space: 7.9 MB (26.86%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -11,8 +11,40 @@ public:
        
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
-     return func(grid.size()-1,grid[0].size()-1,grid.size(),grid[0].size(),grid,dp);   
+     //    vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
+     // return func(grid.size()-1,grid[0].size()-1,grid.size(),grid[0].size(),grid,dp);   
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,0));
+      
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                 if(grid[i][j]==1)
+                 {
+                    dp[i][j]=0;
+                     continue;
+                 }
+                else
+                {
+                    int up=0,dw=0;
+                    if(i>0)up=dp[i-1][j];
+                    if(j>0)dw=dp[i][j-1];
+                    dp[i][j]=up+dw;
+                  
+                }
+                if(i==0&&j==0)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+               
+            }
+        }
+        
+      
+        return dp[m-1][n-1];
        
     }
 };
