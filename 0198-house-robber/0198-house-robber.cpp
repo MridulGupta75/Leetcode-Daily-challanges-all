f2844adbb977d133c5ff55334cdedeb4b5cbc9ed
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-06 15:24:02 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,10 +5,8 @@ public:
         for(int i=0;i<nums.size();i++)
             total[i]=nums[i];
         for(int i=2;i<nums.size();i++)
-        {
-            for(int j=0;j<=i-2;j++)
+               for(int j=0;j<=i-2;j++)
                 total[i]=max(total[j]+nums[i],total[i]);
-        }
         return(*max_element(total.begin(),total.end()));
     }
 };
