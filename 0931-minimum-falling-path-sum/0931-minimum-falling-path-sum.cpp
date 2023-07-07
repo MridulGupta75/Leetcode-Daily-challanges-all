dbfb57db9e25bcf542e05989a6576fac967d37e3
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-07 18:00:03 +0530

    Time: 22 ms (22.79%), Space: 10.6 MB (29.49%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -4,7 +4,7 @@ public:
     {
         if(i>=n||j<0||j>=n)return 10000;
         if(i==n-1)return grid[i][j];
-        if(dp[i][j]!=INT_MIN)return dp[i][j];
+        if(dp[i][j]!=-10001)return dp[i][j];
         int left=func(i+1,j-1,n,grid,dp)+grid[i][j];
         int right=func(i+1,j,n,grid,dp)+grid[i][j];
         int diagonal=func(i+1,j+1,n,grid,dp)+grid[i][j];
@@ -12,7 +12,7 @@ public:
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int mini=INT_MAX;
-        vector<vector<int>>dp(matrix.size(),vector<int>(matrix.size(),INT_MIN));
+        vector<vector<int>>dp(matrix.size(),vector<int>(matrix.size(),-10001));
         for(int j=0;j<matrix.size();j++)
         mini=min(mini,func(0,j,matrix.size(),matrix,dp));
          // mini=min(mini,func(0,1,matrix.size(),matrix));
