8ea9fa838bea51dab093e6e95293cdee8f85f005
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-11 02:45:26 +0530

    Time: 11 ms (95.43%), Space: 6.8 MB (95.30%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -7,11 +7,9 @@ public:
         for(int i=1;i<=l1;i++)
         {
             for(int j=1;j<=l2;j++)
-            {
-                if(word1[i-1]==word2[j-1])curr[j]=1+pre[j-1];
+               if(word1[i-1]==word2[j-1])curr[j]=1+pre[j-1];
                 else curr[j]=max(curr[j-1],pre[j]);
 
-            }
             pre=curr;
         }
         return l1-pre[l2]+l2-pre[l2];
