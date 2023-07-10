930f828834282d7a6beab11ed925478b7f8f1fdd
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-11 02:41:01 +0530

    Time: 38 ms (41.22%), Space: 12.9 MB (64.84%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -14,23 +14,10 @@ public:
         string ans="";
         int i=n,j=m;
         while(j>0&&i>0)
-        {
-            if(str1[i-1]==str2[j-1])
-            {
-                ans+=str1[i-1];
-                i--,j--;
-            }
-            else if(dp[i-1][j]>dp[i][j-1])
-            {
-                ans+=str1[i-1];
-                 i--;
-            }
-            else
-            {
-                ans+=str2[j-1];
-                 j--;
-            }
-        }
+            if(str1[i-1]==str2[j-1]) ans+=str1[i-1],i--,j--;
+            else if(dp[i-1][j]>dp[i][j-1]) ans+=str1[i-1],i--;
+            else ans+=str2[j-1],j--;
+        
         while(i>0)ans+=str1[i-1],i--;
         while(j>0)ans+=str2[j-1],j--;
         reverse(ans.begin(),ans.end());
