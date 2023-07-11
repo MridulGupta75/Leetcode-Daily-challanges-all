5d5f4c42e95f5c6a9df7738fe4e2f9896a821f5f
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-11 18:34:49 +0530

    Time: 3 ms (99.36%), Space: 6.4 MB (93.17%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -11,16 +11,21 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.length(),m=word2.length();
         // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+         // vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         // return func(n-1,m-1,word1,word2,dp);
-        for(int j=0;j<=m;j++)dp[0][j]=j;
-        for(int i=1;i<=n;i++)dp[i][0]=i;
+        vector<int>curr(m+1,0),pre(m+1,0);
+        for(int j=0;j<=m;j++)pre[j]=j;
+         // curr[0]=1;
         
         for(int i=1;i<=n;i++)
+        {
+            curr[0]=i;
         for(int j=1;j<=m;j++)
-            if(word1[i-1]!=word2[j-1]) dp[i][j]=min(1+dp[i-1][j],min(1+dp[i-1][j-1],1+dp[i][j-1]));
-            else dp[i][j]=dp[i-1][j-1];
-        return dp[n][m];
+            if(word1[i-1]!=word2[j-1]) curr[j]=min(1+pre[j],min(1+pre[j-1],1+curr[j-1]));
+            else curr[j]=pre[j-1];
+            pre=curr;
+        }
+        return pre[m];
                 
         
     }
