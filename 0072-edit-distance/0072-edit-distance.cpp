db24b28a5be1b737f137355d1b8ad32fb4690d92
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-11 18:23:39 +0530

    Time: 11 ms (79.60%), Space: 8.8 MB (74.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -13,14 +13,14 @@ public:
         // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
          vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         // return func(n-1,m-1,word1,word2,dp);
-        for(int j=0;j<=m;j++)dp[0][j]=j+1;
-        for(int i=1;i<=n;i++)dp[i][0]=i+1;
+        for(int j=0;j<=m;j++)dp[0][j]=j;
+        for(int i=1;i<=n;i++)dp[i][0]=i;
         
         for(int i=1;i<=n;i++)
         for(int j=1;j<=m;j++)
             if(word1[i-1]!=word2[j-1]) dp[i][j]=min(1+dp[i-1][j],min(1+dp[i-1][j-1],1+dp[i][j-1]));
             else dp[i][j]=dp[i-1][j-1];
-        return dp[n][m]-1;
+        return dp[n][m];
                 
         
     }
