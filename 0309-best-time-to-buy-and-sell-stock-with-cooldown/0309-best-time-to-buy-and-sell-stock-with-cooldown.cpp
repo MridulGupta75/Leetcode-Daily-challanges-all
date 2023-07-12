074261bca6ed1cfb22cc416a635bdfa762da2f5a
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-13 01:06:16 +0530

    Time: 4 ms (76.03%), Space: 11.8 MB (51.76%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -17,7 +17,14 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n,vector(2,-1));
-        return func(0,0,prices,n,dp);
+        // vector<vector<int>>dp(n,vector(2,-1));
+        // return func(0,0,prices,n,dp);
+         vector<vector<int>>dp(n+2,vector(2,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            dp[i][0]=max(-prices[i]+dp[i+1][1],dp[i+1][0]);
+            dp[i][1]=max(prices[i]+dp[i+2][0],dp[i+1][1]);
+        }
+        return dp[0][0];
     }
 };
