eccefd9added7c04be40aa086b0b1771740465ab
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-12 20:42:09 +0530

    Time: 12 ms (37.80%), Space: 13.1 MB (48.69%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    int func(int i,vector<int>& prices,bool b,vector<vector<int>>&dp)
-    {
+//     int func(int i,vector<int>& prices,bool b,vector<vector<int>>&dp)
+//     {
         
-        if(i==prices.size())
-            return 0;
-        int pro=0;
-        if(dp[i][b]!=-1)return dp[i][b];
-        if(!b)
-        {
-             pro=max(-prices[i]+func(i+1,prices,true,dp),func(i+1,prices,false,dp));
-        }
-        else
-        {
-            pro=max(prices[i]+func(i+1,prices,false,dp),func(i+1,prices,true,dp));
-        }
-        return dp[i][b]=pro;
+//         if(i==prices.size())
+//             return 0;
+//         int pro=0;
+//         if(dp[i][b]!=-1)return dp[i][b];
+//         if(!b)
+//         {
+//              pro=max(-prices[i]+func(i+1,prices,true,dp),func(i+1,prices,false,dp));
+//         }
+//         else
+//         {
+//             pro=max(prices[i]+func(i+1,prices,false,dp),func(i+1,prices,true,dp));
+//         }
+//         return dp[i][b]=pro;
         
-    }
+//     }
     int maxProfit(vector<int>& prices) {
         // vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
         // return func(0,prices,false,dp);
        int n=prices.size();
-         vector<vector<int>>dp(n+1,vector<int>(2,0));
+       vector<int>curr(2,0),pre(2,0);
         for(int i=n-1;i>=0;i--)
         {
-            dp[i][0]=max(-prices[i]+dp[i+1][1],dp[i+1][0]);
-        
-                dp[i][1]=max(prices[i]+dp[i+1][0],dp[i+1][1]);
+           curr[0]=max(-prices[i]+pre[1],pre[0]);
+             curr[1]=max(prices[i]+pre[0],pre[1]);
+            pre=curr;
         }
-        return dp[0][0];
+        return pre[0];
         
         
     }
