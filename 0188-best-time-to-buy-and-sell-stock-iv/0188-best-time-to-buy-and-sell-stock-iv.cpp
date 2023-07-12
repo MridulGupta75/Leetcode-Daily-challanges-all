b8b52fcee0dd45a8fe2116bc2c8d601ea9f922c2
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-12 22:03:23 +0530

    Time: 21 ms (61.15%), Space: 16.1 MB (10.77%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -16,9 +16,20 @@ public:
         return dp[i][b][k]=pro;
             
     }
-    int maxProfit(int k, vector<int>& prices) {
+    int maxProfit(int m, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector(2,vector(k+1,-1)));
-        return func(0,0,k,prices,n,dp);
+        // vector<vector<vector<int>>>dp(n,vector(2,vector(k+1,-1)));
+        // return func(0,0,k,prices,n,dp);
+        vector<vector<vector<int>>>dp(n+1,vector(3,vector(m+1,0)));
+        for(int i=n-1;i>=0;i--)
+        {
+            
+                for(int k=0;k<m;k++)
+                {
+                     dp[i][0][k]=max(-prices[i]+dp[i+1][1][k],dp[i+1][0][k]);
+                     dp[i][1][k]=max(prices[i]+dp[i+1][0][k+1],dp[i+1][1][k]);
+                 }
+        }
+        return dp[0][0][0];
     }
 };
