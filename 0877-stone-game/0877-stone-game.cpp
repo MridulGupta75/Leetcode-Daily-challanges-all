f6612147c542dd99c80477a21e12bfed8b6eec95
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-13 23:24:04 +0530

    Time: 37 ms (28.03%), Space: 16.5 MB (21.88%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -13,10 +13,28 @@ public:
         int ans=0;
         for(auto&x:piles)ans+=x;
           int n=piles.size();
-        vector<vector<int>>dp(n,vector(n,-1));
-        int alice=func(0,n-1,piles,dp);
-        int bob=ans-alice;
+        vector<vector<int>>dp(n+3,vector(n+3,0));
+        // int alice=func(0,n-1,piles,dp);
+        // int bob=ans-alice;
+        for(int i=0;i<n;i++)
+        {
+             
+                dp[i][i]=piles[i];
+        }
+           
         
-        return alice>=bob;
+        for(int i=n-1;i>=2;i--)
+        {
+            for(int j=n-1;j>=2;j--)
+            {
+                if(i>j) continue;
+                int c1=piles[i]+min(dp[i+2][j],dp[i+1][j-1]);
+                int c2=piles[j]+min(dp[i][j-2],dp[i+1][j-1]);
+               dp[i][j]=max(c1,c2);
+            }
+        }
+       int alice=dp[0][0];
+        int bob=ans-alice;
+        return alice<=bob;
     }
 };
