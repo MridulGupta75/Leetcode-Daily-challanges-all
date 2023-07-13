eacb749b23a4494dcf3e9401af7bc650f198b67a
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-13 19:03:13 +0530

    Time: 127 ms (31.99%), Space: 7.7 MB (30.56%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    
-    int fun(vector<int>&nums,int i,int j, vector<vector<int>>&dp)
+    int func(int i,int j,vector<int>& nums, vector<vector<int>>&dp)
     {
-        if(i==j)return nums[j];
-          if(dp[i][j]==-1)
-        dp[i][j]= max(nums[i]-fun(nums,i+1,j,dp),nums[j]-fun(nums,i,j-1,dp));
-        return dp[i][j];
+        if(i>j)return 0;
+        if(i==j)return nums[i];
+        // if(dp[i][j]!=-1)return -1;
+       int c1=nums[i]+min(func(i+2,j,nums,dp),func(i+1,j-1,nums,dp));
+       int c2=nums[j]+min(func(i,j-2,nums,dp),func(i+1,j-1,nums,dp));
+        return max(c1,c2);
+        
+        
     }
     bool PredictTheWinner(vector<int>& nums) {
-        int i=0,j=nums.size()-1;
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        if(fun(nums,i,j,dp)>=0)return true;
+        vector<vector<int>>dp(nums.size(),vector(nums.size(),-1));
+        int x= func(0,nums.size()-1,nums,dp);
+        int sum=0;
+        for(auto&x:nums)
+            sum+=x;
+       int p2=sum-x;
+        if(x>=p2)return true;
         return false;
-        
     }
 };
