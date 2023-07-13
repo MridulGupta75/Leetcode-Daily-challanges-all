c132b1dfe73201fb3ae7e3a9ba1608b214804930
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-13 23:27:35 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (80.78%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -12,24 +12,24 @@ public:
     bool stoneGame(vector<int>& piles) {
         int ans=0;
         for(auto&x:piles)ans+=x;
-          int n=piles.size();
-        vector<vector<int>>dp(n+3,vector(n+3,0));
+        //   int n=piles.size();
+        // vector<vector<int>>dp(n+3,vector(n+3,0));
         // int alice=func(0,n-1,piles,dp);
         // int bob=ans-alice;
-        for(int i=0;i<n;i++)
-        dp[i][i]=piles[i];
+//         for(int i=0;i<n;i++)
+//         dp[i][i]=piles[i];
            
         
-        for(int i=n-1;i>=2;i--)
-        {
-            for(int j=n-1;j>=2;j--)
-            {
-                if(i>j) continue;
-                int c1=piles[i]+min(dp[i+2][j],dp[i+1][j-1]);
-                int c2=piles[j]+min(dp[i][j-2],dp[i+1][j-1]);
-               dp[i][j]=max(c1,c2);
-            }
-        }
+//         for(int i=n-1;i>=2;i--)
+//         {
+//             for(int j=n-1;j>=2;j--)
+//             {
+//                 if(i>j) continue;
+//                 int c1=piles[i]+min(dp[i+2][j],dp[i+1][j-1]);
+//                 int c2=piles[j]+min(dp[i][j-2],dp[i+1][j-1]);
+//                dp[i][j]=max(c1,c2);
+//             }
+//         }
        int alice=piles[0];
         int bob=ans-alice;
         return alice<=bob;
