427daa67c26c64c996090af248d6126b7341f57b
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-14 17:37:53 +0530

    Time: 976 ms (27.47%), Space: 292.2 MB (26.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -10,7 +10,20 @@ public:
         return dp[ind][pre+1]=l;
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector(nums.size()+1,-1));
-        return func(0,-1,nums,dp);
+        // vector<vector<int>>dp(nums.size(),vector(nums.size()+1,-1));
+        // return func(0,-1,nums,dp);
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector(n+1,0));
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            for(int pre=ind-1;pre>=-1;pre--)
+            {
+                int l=dp[ind+1][pre+1];
+                if(pre==-1||nums[pre]<nums[ind])
+                    l=max(l,dp[ind+1][ind+1]+1);
+                dp[ind][pre+1]=l;
+            }
+        }
+        return dp[0][0];
     }
 };
