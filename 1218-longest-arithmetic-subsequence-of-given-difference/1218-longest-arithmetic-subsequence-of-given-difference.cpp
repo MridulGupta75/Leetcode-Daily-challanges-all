6d0a75957f4787f05632510f206a4a2f4a13bd19
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-15 00:49:16 +0530

    Time: 288 ms (13.44%), Space: 58.2 MB (8.43%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -21,10 +21,9 @@ public:
             auto itr=mpp.find(arr[i]-difference);
             if(itr!=mpp.end()&&dp[i]<(itr->second)+1) dp[i]=(itr->second)+1;
             auto itr2=mpp.find(arr[i]);
-            if(itr2!=mpp.end())
-                mpp[arr[i]]=max(dp[i],(itr2->second));
-            else
-            mpp.insert({arr[i],dp[i]});
+           
+                mpp[arr[i]]=dp[i];
+            
              
             if(ans<dp[i])ans=dp[i];
         }
