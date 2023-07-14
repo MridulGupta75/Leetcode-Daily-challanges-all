4f9a1cd7137d7acc27a3d0b06f0766d8772fc83f
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-15 00:45:16 +0530

    Time: 302 ms (11.22%), Space: 58.2 MB (10.01%) - LeetHub

---
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int func(int ind,int pre,vector<int>& arr, int d,vector<vector<int>>&dp)
+    {
+        if(ind==arr.size())return 0;
+        if(dp[ind][pre+1]!=-1)return dp[ind][pre+1];
+        int l=func(ind+1,pre,arr,d,dp);
+        if((pre==-1||arr[ind]-arr[pre]==d))
+            l=max(l,1+func(ind+1,ind,arr,d,dp));
+        return dp[ind][pre+1]=l;
+    }
+    int longestSubsequence(vector<int>& arr, int difference) {
+        int n=arr.size();
+        vector<int>dp(n,1);
+        map<int,int>mpp;
+        mpp.insert({arr[0],1});
+        int ans=1;
+        for(int i=1;i<n;i++)
+        {
+            // for(int j=0;j<i;j++)
+            // {
+            auto itr=mpp.find(arr[i]-difference);
+            if(itr!=mpp.end()&&dp[i]<(itr->second)+1)
+                dp[i]=(itr->second)+1;
+            auto itr2=mpp.find(arr[i]);
+            if(itr2!=mpp.end())
+                mpp[arr[i]]=max(dp[i],(itr2->second));
+            else
+            mpp.insert({arr[i],dp[i]});
+                // if((arr[i]-arr[j]==difference)&&dp[i]<dp[j]+1)
+                //     dp[i]=dp[j]+1;
+            // }
+            if(ans<dp[i])ans=dp[i];
+        }
+       
+        return ans;
+       
+        
+    }
+};
