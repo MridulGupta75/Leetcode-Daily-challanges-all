c3b3f799abe66994afddfb1538895d4a636b76c9
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-16 02:45:13 +0530

    Time: 8 ms (57.33%), Space: 8.9 MB (66.65%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -18,7 +18,18 @@ public:
         vector<vector<int>>dp(n,vector(n,-1));
         return func(0,0,triangle,n,dp);
         // vector<vector<int>>dp(n,vector(n,0));
+        for(int j=0;j<n;j++)dp[n-1][j]=triangle[n-1][j];
         
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=n-2;j>=0;j--)
+            {
+                 int l=triangle[i][j]+dp[i+1][j];
+                 int u=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(l,u);
+            }
+        }
+        return dp[0][0];
         
         
     }
