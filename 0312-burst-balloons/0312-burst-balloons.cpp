4da6ca3f382ca9bcd455ba23d6e026ae12de42c9
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-20 01:03:01 +0530

    Time: 445 ms (66.67%), Space: 10.3 MB (72.89%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -16,7 +16,22 @@ public:
         nums.push_back(1);
         nums.insert(nums.begin(),1);
         int n=nums.size();
-        vector<vector<int>>dp(n,vector(n,-1));
-        return func(1,nums.size()-2,nums,dp);
+        // vector<vector<int>>dp(n,vector(n,-1));
+        // return func(1,nums.size()-2,nums,dp);
+        vector<vector<int>>dp(n,vector(n,0));
+        for(int i=n-2;i>=1;i--)
+        {
+            for(int j=i;j<=n-2;j++)
+            {
+                 int mini=INT_MIN;
+                for(int k=i;k<=j;k++)
+                {
+                    int cost=(nums[i-1]*nums[k]*nums[j+1])+dp[i][k-1]+dp[k+1][j];
+                    mini=max(mini,cost);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][n-2];
     }
 };
