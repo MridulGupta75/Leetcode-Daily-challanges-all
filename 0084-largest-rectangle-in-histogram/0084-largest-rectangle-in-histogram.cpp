34ceffe31d88ff6ba5ab978efc766392c417b03c
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-21 16:52:09 +0530

    Time: 236 ms (39.80%), Space: 81.5 MB (47.15%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        stack<int>st;
         int n=heights.size();
-        stack<int>s;
-        int left[n],right[n];
+        vector<int>lt(n,0);
         for(int i=0;i<n;i++)
         {
-            while(!s.empty()&&heights[s.top()]>=heights[i])s.pop();
+
             
-            if(s.empty())left[i]=0;
-            else left[i]=s.top()+1;
-            s.push(i);
+                while(!st.empty()&&heights[i]<=heights[st.top()])
+                {
+                    st.pop();
+                }
+                if(st.empty())lt[i]=0;
+                else lt[i]=st.top()+1;
+                st.push(i);
+            
+        }
+
+        st=stack<int>();
+         vector<int>rt(n,0);
+         for(int i=n-1;i>=0;i--)
+        {
+          
+           
+                while(!st.empty()&&heights[i]<=heights[st.top()])
+                {
+                    st.pop();
+                }
+                if(st.empty())rt[i]=n-1;
+                else rt[i]=st.top()-1;
+                st.push(i);
             
         }
         
-        while(!s.empty())s.pop(); 
-        
-          for(int i=n-1;i>=0;i--)
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++)
         {
-            while(!s.empty()&&heights[s.top()]>=heights[i])s.pop();
-            
-            if(s.empty())right[i]=n-1;
-            else right[i]=s.top()-1;
-            s.push(i);
-            
+            int w=rt[i]-lt[i]+1;
+            maxi=max(w*heights[i],maxi);
         }
         
-        
-        int maxans=INT_MIN;
-        
-        for(int i=0;i<n;i++)
-         maxans=max(maxans,(heights[i]*(abs(left[i]-right[i])+1)));
-        return maxans;
-        
-        
+        return maxi;
         
     }
 };
