501e99e659f9df61729f5b18188fba7233a99da3
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-23 00:25:00 +0530

    Time: 23 ms (50.58%), Space: 8.7 MB (67.87%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    double func(int i,int j,int n,int k,unordered_map<string,double>&dp)
+    double func(int i,int j,int n,int k, vector<vector<vector<double>>>&dp)
     {
         if(i>=n||j>=n||i<0||j<0)return 0;
         if(k==0)return 1;
-        string key=to_string(i)+'_'+to_string(j)+'_'+to_string(k);
-        if(dp.find(key)!=dp.end())return dp[key];
+        if(dp[i][j][k]!=-1)return dp[i][j][k];
         double sum=func(i+1,j-2,n,k-1,dp)+func(i+1,j+2,n,k-1,dp)+func(i-1,j-2,n,k-1,dp)+func(i-1,j+2,n,k-1,dp)+func(i+2,j-1,n,k-1,dp)+func(i+2,j+1,n,k-1,dp)+func(i-2,j-1,n,k-1,dp)+func(i-2,j+1,n,k-1,dp);
         
-        return dp[key]=(double)sum/8;
+        return dp[i][j][k]=(double)sum/8;
     }
     double knightProbability(int n, int k, int row, int column) {
-        // vector<vector<vector<double>>>dp(n,vector(n,vector<double>(k+1,-1)));
-        unordered_map<string,double>mpp;
-        return func(row,column,n,k,mpp);
+        vector<vector<vector<double>>>dp(n,vector(n,vector<double>(k+1,-1)));
+        return func(row,column,n,k,dp);
     }
 };
