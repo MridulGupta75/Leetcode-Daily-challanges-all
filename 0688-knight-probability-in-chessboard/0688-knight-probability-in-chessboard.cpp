8ad85ca31d36c800884891202aef26a06f709c1f
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-23 00:26:01 +0530

    Time: 15 ms (77.15%), Space: 9.2 MB (41.76%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -4,13 +4,13 @@ public:
     {
         if(i>=n||j>=n||i<0||j<0)return 0;
         if(k==0)return 1;
-        if(dp[i][j][k]!=-1)return dp[i][j][k];
+        if(dp[i][k][j]!=-1)return dp[i][k][j];
         double sum=func(i+1,j-2,n,k-1,dp)+func(i+1,j+2,n,k-1,dp)+func(i-1,j-2,n,k-1,dp)+func(i-1,j+2,n,k-1,dp)+func(i+2,j-1,n,k-1,dp)+func(i+2,j+1,n,k-1,dp)+func(i-2,j-1,n,k-1,dp)+func(i-2,j+1,n,k-1,dp);
         
-        return dp[i][j][k]=(double)sum/8;
+        return dp[i][k][j]=(double)sum/8;
     }
     double knightProbability(int n, int k, int row, int column) {
-        vector<vector<vector<double>>>dp(n,vector(n,vector<double>(k+1,-1)));
+        vector<vector<vector<double>>>dp(n,vector(k+1,vector<double>(n,-1)));
         return func(row,column,n,k,dp);
     }
 };
