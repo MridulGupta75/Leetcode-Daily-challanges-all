a6f848e829a1d463f6788bb35935b6e4fe553c7a
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-25 01:49:29 +0530

    Time: 4 ms (14.05%), Space: 6.2 MB (13.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,7 +10,15 @@ public:
        
    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return func(n,dp);
+        vector<int>dp(n+1,0);
+        // return func(n,dp);
+        dp[0]=1,dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            int sol=dp[i-1]+dp[i-2];
+             dp[i]=sol;
+        }
+        return dp[n];
+        
     }
 };
