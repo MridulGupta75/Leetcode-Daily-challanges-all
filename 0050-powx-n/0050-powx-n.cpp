6ba1b30cb2f32dd7366606c50342a3d3cbcdc105
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-25 01:20:44 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (92.19%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -6,24 +6,14 @@ public:
         bool nflag=false;
         bool xflag=false;
         if(k<0)nflag=true;
-     
         long long n=abs(k);
         if(x<0&&n%2) xflag=true;
         double y=abs(x);
     
         while(n>0)
-        {
-            if(n%2)
-            {
-                sol*=y;
-                n=n-1;
-            }
-            else
-            {
-                y=y*y;
-                n=n/2;
-            }
-        }
+           if(n%2) sol*=y,n=n-1;
+            else  y=y*y,n=n/2;
+        
         if(xflag)sol*=-1;
         return nflag?1.0/sol:sol;
     }
