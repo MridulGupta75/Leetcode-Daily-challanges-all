583256d64a7ddec897d7d6e00ba0bbe73b61aeda
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-30 01:19:51 +0530

    Time: 7 ms (63.98%), Space: 138.5 MB (13.41%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
+    vector<pair<int,int>>par={{100,0},{75,25},{50,50},{25,75}};
     double func(int a,int b,vector<vector<double>>&dp)
     {
         if(a<=0&&b<=0)return 0.5;
         if(a<=0)return 1.0;
         if(b<=0)return 0;
         if(dp[a][b]!=-1)return dp[a][b];
-        double p1=func(a-100,b,dp);
-        double p2=func(a-75,b-25,dp);
-        double p3=func(a-50,b-50,dp);
-        double p4=func(a-25,b-75,dp);
-        
-        return dp[a][b]=(0.25*(p1+p2+p3+p4));
+        double prob=0.0;
+        for(auto&x:par) prob+=func(a-x.first,b-x.second,dp);   
+        return dp[a][b]=(0.25*prob);
     }
     double soupServings(int n) {
         if(n>=4000)return 1;
