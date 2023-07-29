ad1dc48ee40a9dda731d9cf3484b9c5c2b33d82e
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-07-29 21:12:14 +0530

    Time: 404 ms (99.65%), Space: 179.6 MB (9.61%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,123 +1,97 @@
-// class LRUCache {
-// public:
-//     list <int>ls;
-//     map<int, list<int>::iterator>mpp;
-//     int cap;
-//     LRUCache(int capacity) {
-//         cap=capacity;
-//     }
-    
-//     int get(int key) {
-//         auto itr=mpp.find(key);
-//         if(itr!=mpp.end())
-//         {
-
-//                auto it1=itr->second;
-//              mpp.erase(itr);
-//             ls.erase(it1);
-
-//             ls.push_back(key);
-//             mpp[key]=--ls.end();
-//             return key;
-            
-//         }
-//        return -1;
-        
-//     }
-    
-//     void put(int key, int value) {
-//         if(mpp.size()<cap)
-//         {
-//         auto itr=mpp.find(key);
-//         if(itr!=mpp.end())
-//         {
-//             ls.erase(itr->second);
-//                 mpp.erase(key);  
-//             ls.push_back(key);
-             
-                  
-//         }
-//             else
-//             {
-//             ls.push_back(key);
-                  
-               
-//             }
-            
-//         }
-//         else
-//         {
-//             mpp.erase(key);
-//            ls.pop_front();
-            
-//             ls.push_back(key);
-              
-//         }
-          
-//          auto itr = ls.end();
-//             mpp.insert({key,--itr});  
-     
-        
-        
-//     }
-// };
-
-// /**
-//  * Your LRUCache object will be instantiated and called as such:
-//  * LRUCache* obj = new LRUCache(capacity);
-//  * int param_1 = obj->get(key);
-//  * obj->put(key,value);
-//  */
-
-
-
-
-
-
 class LRUCache {
 public:
-    int caps;
-    list<pair<int,int>> mylist;
-    unordered_map<int,list<pair<int,int>>::iterator> mymap;
+    typedef struct node{
+     struct node*next;
+     int val;
+      int key;
+     struct node*back;
+    }nt;
+    
+    unordered_map<int,nt*>mpp;
+        
+    int cap;
+    nt*head=NULL,*tail=NULL;
+    
+    
     LRUCache(int capacity) {
-        caps = capacity;
+        cap=capacity;
+       head=new nt();
+       tail=new nt();
+        head->val=-1;
+        tail->val=-1;
+         head->key=-1;
+        tail->key=-1;
+        head->next=tail;
+        tail->back=head;
+        tail->next=NULL;
+        head->back=NULL;
+        
     }
     
     int get(int key) {
-        auto it = mymap.find(key);
-        if(it == mymap.end())
-        {
-            return -1;
-        }
-        auto it1 = it->second;
-        mymap.erase(key);
-        int val = it1->second;
-        mylist.erase(it1);
-        mylist.push_back({key,val});
-        auto itr = mylist.end();
-        mymap.insert({key,--itr});
+        auto it=mpp.find(key);
+        if(it==mpp.end())return -1;
+        nt*temp=it->second;
+        int val=temp->val;
+        int k=temp->key;
+        temp->next->back=temp->back;
+        temp->back->next=temp->next;
+        delete(temp);
+        nt*t=new nt();
+        t->val=val;
+        t->key=k;
+        nt*hh=head->next;
+        head->next=t;
+        t->next=hh;
+        hh->back=t;
+        t->back=head;
+        mpp[key]=t;
+        
         return val;
     }
     
     void put(int key, int value) {
-        auto it = mymap.find(key);
-        if(it == mymap.end())
+        
+        
+        auto it=mpp.find(key);
+        if(it!=mpp.end())
         {
-            pair<int,int> p = mylist.front();
-            if(mylist.size() == caps)
-            {
-                mylist.pop_front();
-                mymap.erase(p.first);
-            }  
+             nt*temp=it->second;
+            int k=temp->key;
+              temp->next->back=temp->back;
+             temp->back->next=temp->next;
+              delete(temp);
+              mpp.erase(k);
         }
-        else
+        else if(mpp.size()==cap)
         {
-            auto it1 = it->second;
-            mylist.erase(it1);
-            mymap.erase(key);
+            nt*temp=tail->back;
+            int k=temp->key;
+              temp->next->back=temp->back;
+             temp->back->next=temp->next;
+              delete(temp);
+              mpp.erase(k);
+             
         }
-        mylist.push_back({key,value});
-        auto itr = mylist.end();
-        mymap.insert({key,--itr});
+        
+            nt*t=new nt();
+            t->val=value;
+            t->key=key;
+            nt*hh=head->next;
+            head->next=t;
+            t->next=hh;
+            hh->back=t;
+            t->back=head;
+            mpp[key]=t;
+            
+        
+        
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
