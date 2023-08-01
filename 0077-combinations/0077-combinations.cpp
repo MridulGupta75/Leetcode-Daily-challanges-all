531a0204998d7daf26d99a74d1967a1f26872a7d
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-01 14:34:13 +0530

    Time: 36 ms (36.49%), Space: 17.9 MB (31.99%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void func(int i,int k,vector<vector<int>>&sol,vector<int>&subsol,int n)
+    {
+        if(i>n+1)return ;
+        if(subsol.size()==k)
+        {
+            sol.push_back(subsol);
+            return ;
+        }
+        //take
+        subsol.push_back(i);
+        func(i+1,k,sol,subsol,n);
+        subsol.pop_back();
+        //not take
+        func(i+1,k,sol,subsol,n);   
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> sol;
+        vector<int>subsol;
+        func(1,k,sol,subsol,n);
+        return sol;
+        
+    }
+};
