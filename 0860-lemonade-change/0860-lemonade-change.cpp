079f2b6eb3cc33d94736b710775e446a37099fac
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-01 22:30:07 +0530

    Time: 88 ms (96.35%), Space: 83.3 MB (65.06%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        vector<int>dp(3,0);
+       int k5=0,k10=0,k20=0;
         if(bills[0]>5)return false;
         for(int i=0;i<bills.size();i++)
         {
-            if(bills[i]==5)dp[0]++;
+            if(bills[i]==5)k5++;
             
             else if(bills[i]==10)
             {
-                if(dp[0]>=1)dp[0]--;
+                if(k5>=1)k5--;
                 else return false;
-                dp[1]++;
+                k10++;
             }
             else
             {
-                if((dp[1]>=1&&dp[0]>=1))dp[0]--,dp[1]--;
-                else if((dp[0]>=3))dp[0]-=3; 
+                if((k10>=1&&k5>=1))k5--,k10--;
+                else if((k5>=3))k5-=3; 
                 else return false;
-                dp[2]++;
+                k20++;
             }
         }
         return true;
