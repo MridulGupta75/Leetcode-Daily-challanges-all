7396e57327bc216221f0ceb687e84c39d37432cd
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-02 20:49:44 +0530

    Time: 14 ms (82.75%), Space: 16.6 MB (62.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -8,14 +8,19 @@ public:
         if(dp[ind]!=-1)return dp[ind];
         int mini=1e5;
         for(int i=1;i<=nums[ind];i++)
-        {
-            mini=min(mini,1+func(i+ind,nums,dp)); 
-        }
+        mini=min(mini,1+func(i+ind,nums,dp)); 
         
         return dp[ind]=mini;
     }
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
-        return func(0,nums,dp);
+        // vector<int>dp(nums.size()+1,-1);
+        // return func(0,nums,dp);
+        int far=0,jump=0,curr=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+         far=max(far,nums[i]+i);
+            if(i==curr)jump++,curr=far;
+        }
+        return jump;
     }
 };
