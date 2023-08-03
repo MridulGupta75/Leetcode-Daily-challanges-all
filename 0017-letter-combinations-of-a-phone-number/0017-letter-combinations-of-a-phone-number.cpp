92d572f442b5c15402dddd481876b1115948d929
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-03 14:09:51 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.40%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
       unordered_map<char,string>mpp{{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
      vector<string>sol;
-    void func(int ind,int strind,string&digits,string&substr)
+    void func(int strind,string&digits,string&substr)
     {
         
         if(substr.size()==digits.size())sol.push_back(substr);
-        // if(substr.size()>digits.size())return;    
         string temp=mpp[digits[strind]];
-        for(int i=ind;i<temp.size();i++)
+        for(int i=0;i<temp.size();i++)
         {
             substr.push_back(temp[i]);
-            func(ind,strind+1,digits,substr);
+            func(strind+1,digits,substr);
             substr.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
         if(digits.size()==0)return sol;
        string substr="";
-       func(0,0,digits,substr);
+       func(0,digits,substr);
        return sol;
     }
 };
