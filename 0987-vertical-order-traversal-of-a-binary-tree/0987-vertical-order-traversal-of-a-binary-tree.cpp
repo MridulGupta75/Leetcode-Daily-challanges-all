8c5c5e477626a2f3c92bac727e756d775862f706
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-06 14:26:03 +0530

    Time: 4 ms (68.99%), Space: 12.5 MB (80.51%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,47 +12,34 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        if(root==NULL)return {};
-        queue<pair<TreeNode*,pair<int,int>>>q;
+        if(root==NULL)return{};
+        queue<pair<pair<TreeNode*,int>,int>>q;
         map<int,map<int,multiset<int>>>mpp;
-        q.push({root,{0,0}});
+        q.push({{root,0},0});
         while(!q.empty())
         {
             int size=q.size();
             for(int i=0;i<size;i++)
             {
-                TreeNode*node=q.front().first;
-                int x=q.front().second.first;
-                int y=q.front().second.second;
-                int k=node->val;
+                TreeNode*node=q.front().first.first;
+                int ht=q.front().first.second;
+                int level=q.front().second;
                 q.pop();
-               mpp[x][y].insert(k);
-                if(node->left!=NULL)
-                {
-                    q.push({node->left,{x-1,y+1}});
-                }
-                 if(node->right!=NULL)
-                {
-                    q.push({node->right,{x+1,y+1}});
-                }
-                
+                mpp[ht][level].insert(node->val);
+                if(node->left!=NULL)q.push({{node->left,ht-1},level+1});
+                if(node->right!=NULL)q.push({{node->right,ht+1},level+1});
             }
         }
-        vector<vector<int>>sol;
+         vector<vector<int>>sol;
         for(auto&x:mpp)
         {
             vector<int>subsol;
-          for(auto&y:x.second)
-          {
-              subsol.insert(subsol.end(),y.second.begin(),y.second.end());
-          }
+            for(auto&y:x.second)
+            {
+                 subsol.insert(subsol.end(),y.second.begin(),y.second.end());
+            }
             sol.push_back(subsol);
-
-            
-               // cout<<x.first<<" "<<endl;
         }
-
         return sol;
-        
     }
 };
