b5cfdd124d24d007ed246464a71332f30eb19ebf
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-09 17:19:34 +0530

    Time: 12 ms (96.93%), Space: 17.7 MB (66.99%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>lt(n,1);
+        for(int i=1;i<n;i++)
+        if(ratings[i]>ratings[i-1])lt[i]=lt[i-1]+1;
+        int right=1;
+        for(int i=n-2;i>=0;i--)
+            if(ratings[i]>ratings[i+1])right++,lt[i]=max(lt[i],right);
+            else right=1;
+        int sum=0;
+        for(int i=0;i<n;i++)
+            sum+=lt[i];
+        return sum;
+            
+    }
+};
