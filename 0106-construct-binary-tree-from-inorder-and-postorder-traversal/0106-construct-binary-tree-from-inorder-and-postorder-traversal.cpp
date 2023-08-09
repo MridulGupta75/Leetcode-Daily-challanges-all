3222d9b37590b7648054a3fab5c665abd7b5a6d0
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-10 00:48:28 +0530

    Time: 22 ms (51.26%), Space: 26.1 MB (68.85%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -9,24 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int i=postorder.size()-1;
-        return(tree(inorder,postorder,i,0,inorder.size()-1));
+// class Solution {
+// public:
+//     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+//         int i=postorder.size()-1;
+//         return(tree(inorder,postorder,i,0,inorder.size()-1));
         
+//     }
+    
+//      TreeNode* tree(vector<int>& inorder, vector<int>& postorder,int&i,int l,int r)
+//      {
+//          if(l>r)return NULL;
+//          int x=r;
+//          while(postorder[i]!=inorder[x]) x--;
+//          i--;
+//          TreeNode* root=new TreeNode(inorder[x]);
+//          root->right=tree(inorder,postorder,i,x+1,r);
+//          root->left=tree(inorder,postorder,i,l,x-1);
+//          return root;
+//      }
+    
+// };
+
+class Solution {
+private:
+   int findIndex(int val,vector<int>&inorder,int start,int end)
+   {
+       for(int i=start;i<=end;i++)
+           if(inorder[i]==val)return i;
+       return -1;
+   } 
+    TreeNode* makeTree(TreeNode*root , int&pre_idx , vector<int>&inorder, int start , int end , vector<int>&preorder)
+    {
+        if(pre_idx<0)return NULL;
+        int idx=findIndex(preorder[pre_idx],inorder,start,end);
+        if(idx==-1)return NULL;
+        root=new TreeNode(preorder[pre_idx]),pre_idx--;
+        root->right=makeTree(root->right,pre_idx,inorder,idx+1,end,preorder);
+        root->left=makeTree(root->left,pre_idx,inorder,start,idx-1,preorder);
+        return root;
     }
     
-     TreeNode* tree(vector<int>& inorder, vector<int>& postorder,int&i,int l,int r)
-     {
-         if(l>r)return NULL;
-         int x=r;
-         while(postorder[i]!=inorder[x]) x--;
-         i--;
-         TreeNode* root=new TreeNode(inorder[x]);
-         root->right=tree(inorder,postorder,i,x+1,r);
-         root->left=tree(inorder,postorder,i,l,x-1);
-         return root;
-     }
-    
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int pre=postorder.size()-1;
+        return makeTree(NULL,pre,inorder,0,inorder.size()-1,postorder);
+        
+    }
 };
