2b8822b6012399e33120fa251737a67d059bb811
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-09 20:58:30 +0530

    Time: 9 ms (37.79%), Space: 13.1 MB (41.14%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -10,6 +10,7 @@
 class Solution {
 private:
     unordered_map<TreeNode*,TreeNode*>parent;
+    //sara node ka parent save karna ka lia map ma ya func  h
     void getParents(TreeNode*root)
     {
         parent[root]=NULL;
@@ -39,29 +40,29 @@ private:
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         getParents(root);
-        set<TreeNode*>visited;
-          queue<TreeNode*>q;
+        set<TreeNode*>visited;/* set jo search karaga ki available h ya ni set m wo node jiska base pa pata chalaga ki visited h ya                                                            ni  wo*/
+          queue<TreeNode*>q;//bsf ka lia queue
         q.push(target);
-        visited.insert(target);
-          while(k--)
+        visited.insert(target);//initially dono queue and set m dalsia
+          while(k--)//distatance 0 hota he break
           {
-              int size=q.size();
+              int size=q.size();//har iteration ka lia size queue ka
               
               for(int i=0;i<size;i++)
               {
                   TreeNode*node=q.front();
                   q.pop(); 
-                  if(node->left&&visited.find(node->left)==visited.end())
+                  if(node->left&&visited.find(node->left)==visited.end())//check left NULL tw ni fir vistited tw ni fir insert
                       q.push(node->left),visited.insert(node->left);
-                   if(node->right&&visited.find(node->right)==visited.end())
+                   if(node->right&&visited.find(node->right)==visited.end())//same as left
                       q.push(node->right),visited.insert(node->right);
                   TreeNode*temp=parent[node];
-                  if(temp&&visited.find(temp)==visited.end())
+                  if(temp&&visited.find(temp)==visited.end())//check baap NULL tw ni fir uska baap visited tw ni fir insert
                        q.push(temp),visited.insert(temp);
                   
               }
           }
-        vector<int>sol;
+        vector<int>sol;  // baki queue ko nikal ka print kardo simple jo lat m a h
         while(!q.empty())
         {
             sol.push_back(q.front()->val);
