e25fa6172e1c4add5d6cc5bab7af1c9f98fb29d3
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-10 15:45:49 +0530

    Time: 3 ms (91.34%), Space: 13.9 MB (50.05%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    private:
+    bool binarySearch(vector<int>&nums,int target,int l,int r)
+    {
+        while(l<=r)
+        {
+             int mid=l+(r-l)/2;
+            if(nums[mid]==target)return true;
+            else if(nums[mid]>target)r=mid-1;
+            else l=mid+1;
+        }
+        return false;
+    }
+public:
+    bool search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        while(l<r)
+        {
+            while(l<r&&nums[l]==nums[l+1])l++;
+            while(l<r&&nums[r]==nums[r-1])r--;
+             int mid=l+(r-l)/2;
+             if(nums[mid]>nums[r])l=mid+1;
+             else r=mid;
+        }
+        return(binarySearch(nums,target,0,l-1)|binarySearch(nums,target,l,nums.size()-1));
+    }
+};
