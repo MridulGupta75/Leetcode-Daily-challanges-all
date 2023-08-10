3a6fa987da7ad13a6f961db04c92db086238e317
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-11 00:33:40 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (30.66%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -3,11 +3,9 @@ public:
     bool check(vector<int>& nums) {
         int c=0;
         for(int i=0;i<nums.size()-1;i++)
-        {
-            if(nums[i]>nums[i+1])c++;
-        }
-        if(nums[nums.size()-1]>nums[0])c++;
+        if(nums[i]>nums[i+1])c++;
         
+        if(nums[nums.size()-1]>nums[0])c++;
         return c<=1?true:false;
     }
 };
