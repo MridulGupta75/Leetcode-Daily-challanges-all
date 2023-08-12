a82c1bbd8ba640a2584969fa76155dc138ccd36d
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-12 14:32:17 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,47 +1,18 @@
 class Solution {
+private:
+int findWays(int i,int j,int m,int n,vector<vector<int>>& Mat,vector<vector<int>>&dp)  
+{
+    if(i>=m||j>=n)return 0;
+    if(i==m-1&&j==n-1)return 1;
+    if(Mat[i][j]==1)return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
+    return dp[i][j]=findWays(i,j+1,m,n,Mat,dp)+findWays(i+1,j,m,n,Mat,dp);
+}
 public:
-    int func(int i,int j,int m,int n,vector<vector<int>>& grid,vector<vector<int>>&dp)
-    {
+    int uniquePathsWithObstacles(vector<vector<int>>& Mat) {
+        if(Mat[Mat.size()-1][Mat[0].size()-1]==1)return 0;
+        vector<vector<int>>dp(Mat.size(),vector<int>(Mat[0].size(),-1));
+        return findWays(0,0,Mat.size(),Mat[0].size(),Mat,dp);
         
-        if(i<0||j<0)return 0;
-        if(grid[i][j]==1)return 0;
-        if(i==0&&j==0)return 1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        return (dp[i][j]=(func(i-1,j,m,n,grid,dp)+func(i,j-1,m,n,grid,dp)));
-       
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-     //    vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
-     // return func(grid.size()-1,grid[0].size()-1,grid.size(),grid[0].size(),grid,dp);   
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));
-      
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                 if(grid[i][j]==1)
-                 {
-                    dp[i][j]=0;
-                     continue;
-                 }
-                else
-                {
-                    int up=0,dw=0;
-                    if(i>0)up=dp[i-1][j];
-                    if(j>0)dw=dp[i][j-1];
-                    dp[i][j]=up+dw;
-                  
-                }
-                if(i==0&&j==0)
-                dp[i][j]=1;
-               
-            }
-        }
-        
-      
-        return dp[m-1][n-1];
-       
     }
 };
