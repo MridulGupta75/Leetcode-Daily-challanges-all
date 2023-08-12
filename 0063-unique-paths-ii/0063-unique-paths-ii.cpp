16e1118234a97d55da857b4a4905cba7fb157a14
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-12 15:13:49 +0530

    Time: 5 ms (28.03%), Space: 7.9 MB (6.12%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
-private:
-int findWays(int i,int j,int m,int n,vector<vector<int>>& Mat,vector<vector<int>>&dp)  
-{
-    if(i>=m||j>=n)return 0;
-    if(i==m-1&&j==n-1)return 1;
-    if(Mat[i][j]==1)return 0;
-    if(dp[i][j]!=-1)return dp[i][j];
-    return dp[i][j]=findWays(i,j+1,m,n,Mat,dp)+findWays(i+1,j,m,n,Mat,dp);
-}
 public:
+    int func(int m,int n,vector<vector<int>>&Mat, vector<vector<int>>&dp)
+    {
+        if(m<0||n<0)return 0;
+        if(m==0&&n==0)return 1;
+        if(Mat[m][n]==1)return 0;
+        if(dp[m][n]!=-1)return dp[m][n];
+        return dp[m][n]=func(m-1,n,Mat,dp)+func(m,n-1,Mat,dp);
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& Mat) {
         int m=Mat.size(),n=Mat[0].size();
-        if(Mat[m-1][n-1]==1)return 0;
+        if(Mat[0][0]==1)return 0;
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return findWays(0,0,Mat.size(),Mat[0].size(),Mat,dp);
+        return func(m-1,n-1,Mat,dp);
         
     }
 };
