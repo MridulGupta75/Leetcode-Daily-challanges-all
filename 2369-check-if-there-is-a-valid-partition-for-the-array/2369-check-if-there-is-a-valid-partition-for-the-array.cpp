3595caf223644582f2a5c89e1ee0b5ddfd75a620
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-13 17:39:56 +0530

    Time: 125 ms (91.38%), Space: 84 MB (85.85%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -29,7 +29,46 @@ private:
 public:
     bool validPartition(vector<int>& nums) {
         n=nums.size();
-        vector<int>dp(nums.size(),-1);
-        return func(0,nums,dp);
+        // vector<int>dp(nums.size(),-1);
+        // return func(0,nums,dp);
+        vector<bool>dp(n+1,0);
+        dp[n]=true;
+        for(int index=n-1;index>=0;index--)
+        {
+        bool result = false;
+        //rule1 
+        if(index+1<n&&nums[index]==nums[index+1]) 
+        {
+            result |=dp[index+2];
+        }
+        if(result)
+        {
+            dp[index]=true;
+            continue;
+        }
+        //rule 2
+        if(index+2<n&&nums[index]==nums[index+1]&&nums[index+1]==nums[index+2])
+            result |=dp[index+3];
+        
+        if(result)
+        {
+            dp[index]=true;
+            continue;
+        }
+         
+        //rule 3
+        if(index+2<n&&nums[index+1]-nums[index]==1&&nums[index+2]-nums[index+1]==1) 
+            result |=dp[index+3];
+        
+        if(result)
+        {
+            dp[index]=true;
+            continue;
+        }
+        dp[index]=false;
+        }
+        return dp[0];
+        
+        
     }
 };
