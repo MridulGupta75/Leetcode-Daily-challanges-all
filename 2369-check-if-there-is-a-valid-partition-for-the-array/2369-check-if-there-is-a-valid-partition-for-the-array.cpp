bc19ae2ad881e6aa2ae39c0aaba87055575073c7
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-13 17:31:19 +0530

    Time: 136 ms (79.69%), Space: 93 MB (22.46%) - LeetHub

---
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int n;
+    
+    bool func(int index,vector<int>&nums,vector<int>&dp)
+    {
+        if(index>=n)return true;
+        if(dp[index]!=-1)return dp[index];
+        bool result = false;
+        //rule1 
+        if(index+1<n&&nums[index]==nums[index+1]) 
+        {
+            result |=func(index+2,nums,dp);
+        }
+        if(result)return dp[index]=true;
+        //rule 2
+        if(index+2<n&&nums[index]==nums[index+1]&&nums[index+1]==nums[index+2])
+            result |=func(index+3,nums,dp);
+        
+        if(result)return dp[index]=true;
+         
+        //rule 3
+        if(index+2<n&&nums[index+1]-nums[index]==1&&nums[index+2]-nums[index+1]==1) result |=func(index+3,nums,dp);
+        
+        if(result)return dp[index]=true;
+        return dp[index]=false;
+        
+    }
+public:
+    bool validPartition(vector<int>& nums) {
+        n=nums.size();
+        vector<int>dp(nums.size(),-1);
+        return func(0,nums,dp);
+    }
+};
