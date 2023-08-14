7408b4de617b40b39547b31fdc43cd3b418b54d9
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-14 23:52:09 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (21.63%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int pos=-1;
-        for(int i=nums.size()-1;i>0;i--)
+                       //     4
+// need this pos of 2 //  3       2(kam)
+                      // 1         5(jada)
+        for(int i=nums.size()-1;i>=1;i--)
+        if(nums[i]>nums[i-1])
         {
-            if(nums[i]>nums[i-1])
-            {
-                pos=i-1;
-                break;
-            }
+            pos=i-1;
+            break;
         }
-         if(pos==-1)
+        
+        if(pos==-1) 
         {
             reverse(nums.begin(),nums.end());
-             return;
+            return;
         }
-        for(int i=nums.size()-1;i>pos;i--)
+        
+        for(int i=nums.size()-1;i>pos;i--) 
         {
             if(nums[i]>nums[pos])
             {
-               swap(nums[i],nums[pos]);
-                break;
+                 swap(nums[i],nums[pos]);
+                 break;
             }
         }
-       
-    
-            int i=pos+1,j=nums.size()-1;
-          while(i<j)
-            {
-                swap(nums[i++],nums[j--]);
-            }
+        reverse(nums.begin()+pos+1,nums.end());
         
         
     }
