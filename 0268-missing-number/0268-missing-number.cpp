0fc2fb97f550abb742cb5365dc44de83c84ba8f7
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-14 17:29:36 +0530

    Time: 12 ms (89.92%), Space: 18 MB (13.12%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        int sum=(n*(n+1))/2;
-        int sumof_array=0;
-        for(auto&x:nums)sumof_array+=x;
-        return sum-sumof_array;
+        // int n=nums.size();
+        // int sum=(n*(n+1))/2;
+        // int sumof_array=0;
+        // for(auto&x:nums)sumof_array+=x;
+        // return sum-sumof_array;
+        int xor1=0,xor2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            xor2=xor2^nums[i];
+            xor1=xor1^(i+1);
+        }
+        // xor1=xor1^(nums.size());
+        return xor1^xor2;
         
     }
 };
