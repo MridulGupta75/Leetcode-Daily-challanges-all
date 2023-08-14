c62f6917e685e396bd04620b2eab43fe55b68272
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-14 15:22:45 +0530

    Time: 15 ms (99.01%), Space: 24.9 MB (63.38%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        deque<int>q;
-        for(auto&x:nums)q.push_back(x);
-       
-        while(k--)
-        {
-            q.push_front(q.back());
-            q.pop_back();
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            nums[i]=q.front();
-            q.pop_front();
-        }
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
