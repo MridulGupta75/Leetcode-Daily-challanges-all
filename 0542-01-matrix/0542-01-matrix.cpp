673f06c596474c409abd92e72652ec2db332ac5d
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-17 18:54:42 +0530

    Time: 128 ms (14.26%), Space: 45.6 MB (11.22%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    vector<pair<int,int>>pr{{-1,0},{0,1},{0,-1},{1,0}};
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        vector<vector<int>>sol(mat.size(),vector<int>(mat[0].size(),-1));
+         queue<vector<int>>q;
+        for(int i=0;i<mat.size();i++)
+        {
+            for(int j=0;j<mat[0].size();j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    
+                    sol[i][j]=0;
+                    q.push({i,j});
+                }
+              
+            }
+        }
+                   while(!q.empty())
+                    {
+                        int l=q.front()[0];
+                        int m=q.front()[1];
+                        // int wt=q.front()[2];
+                         q.pop();
+                        for(auto&x:pr)
+                        {
+                            int ll=l+x.first;
+                            int mm=m+x.second;
+
+                            if(ll>=0&&ll<mat.size()&&mm>=0&&mm<mat[0].size()&&sol[ll][mm]==-1)
+                            {
+                                sol[ll][mm]=sol[l][m]+1;
+                                q.push({ll,mm});
+                            }
+                             
+        
+                        }
+                        
+                    }//while
+        return sol;
+        
+    }
+};
