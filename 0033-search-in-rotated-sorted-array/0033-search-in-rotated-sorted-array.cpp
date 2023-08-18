c537ffac43a2d390f2d65284e167ad79aee98a68
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-18 22:14:13 +0530

    Time: 3 ms (70.09%), Space: 11.1 MB (18.70%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -13,17 +13,38 @@ class Solution {
     }
 public:
     int search(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1;
-        while(l<r)
-        {
-            int mid=l+(r-l)/2;
-            if(nums[mid]>nums[r])l=mid+1;
-            else r=mid;
-        }
-        int ind1=bs(nums,0,l-1,target);
-        int ind2=bs(nums,l,nums.size()-1,target);
-        return ind1==-1?ind2:ind1;
+        // int l=0,r=nums.size()-1;
+        // while(l<r)
+        // {
+        //     int mid=l+(r-l)/2;
+        //     if(nums[mid]>nums[r])l=mid+1;
+        //     else r=mid;
+        // }
+        // int ind1=bs(nums,0,l-1,target);
+        // int ind2=bs(nums,l,nums.size()-1,target);
+        // return ind1==-1?ind2:ind1;
         
+        
+        //striver
+        int l=0,h=nums.size()-1;
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target)return mid;
+            //left is sorted
+            if(nums[l]<=nums[mid])
+            {
+                if(nums[l]<=target&&nums[mid]>=target)h=mid;
+                else l=mid+1;
+            }
+            else
+            {
+                if(nums[mid]<=target&&nums[h]>=target)l=mid;
+                else h=mid-1;
+            }
+        }
+        return -1;
         
     }
 };
