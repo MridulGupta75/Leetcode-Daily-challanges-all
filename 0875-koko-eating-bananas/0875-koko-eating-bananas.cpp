f8045bc95df300d55e0fbaa41b3b14a6ced1e88e
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-20 17:58:48 +0530

    Time: 32 ms (91.30%), Space: 18.9 MB (82.07%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    private:
+    bool eat(int k,vector<int>&piles,int h)
+    {
+        int c=0;
+        for(auto&x:piles)
+        {
+            if(x!=k)
+            {
+                int d=(x/k);
+                if(x%k!=0)
+                 c=c+d+1;
+                else
+                    c+=d;
+                
+            }
+            else
+            {
+                c++;
+            }
+             if(c>h)return false;
+            
+        }
+        return true;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1;
+        int high=INT_MAX;
+        int min;
+        while(l<=high)
+        {
+            int mid=l+(high-l)/2;
+            if(eat(mid,piles,h))
+            {
+                min=mid;
+                high=mid-1;
+            }
+            else l=mid+1;
+        }
+        return min;
+        
+    }
+};
