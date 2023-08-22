6500554bbef4cf95169c4676e6076eb2b9297ceb
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-22 15:19:05 +0530

    Time: 2 ms (49.78%), Space: 5.9 MB (37.93%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -7,11 +7,13 @@ public:
         while(cn>26)
         {
             int rem=cn%26;
-            str=ch[rem]+str;
+            if(rem==0)str='Z'+str;
+            else
+            str=(char)(rem-1+'A')+str;
             cn=cn/26;
             if(rem==0)cn--;
         }
-       str=ch[cn]+str;
+       str=(char)(cn-1+'A')+str;
         return str;
     }
 };
