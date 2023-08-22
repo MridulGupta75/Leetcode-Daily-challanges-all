59362c9d627c239a2ad14c89a0f8c998c29335c8
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-22 15:20:00 +0530

    Time: 2 ms (49.78%), Space: 5.9 MB (37.93%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,6 +1,4 @@
 class Solution {
-    private:
-    vector<char>ch{'Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 public:
     string convertToTitle(int cn) {
         string str="";
@@ -8,8 +6,7 @@ public:
         {
             int rem=cn%26;
             if(rem==0)str='Z'+str;
-            else
-            str=(char)(rem-1+'A')+str;
+            else str=(char)(rem-1+'A')+str;
             cn=cn/26;
             if(rem==0)cn--;
         }
