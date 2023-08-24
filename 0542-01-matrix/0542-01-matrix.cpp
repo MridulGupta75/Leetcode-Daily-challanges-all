9aedd16c3c5aa8b92681db0fd6497530b516e36b
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-24 16:44:22 +0530

    Time: 153 ms (8.68%), Space: 58.4 MB (5.14%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,44 +1,38 @@
 class Solution {
-private:
-    vector<pair<int,int>>pr{{-1,0},{0,1},{0,-1},{1,0}};
+    private:
+    vector<pair<int,int>>pr{{-1,0},{0,-1},{1,0},{0,1}};
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        vector<vector<int>>sol(mat.size(),vector<int>(mat[0].size(),-1));
-         queue<vector<int>>q;
+        queue<vector<int>>q;
+        vector<vector<int>>vis(mat.size(),vector<int>(mat[0].size(),0));
+         vector<vector<int>>sol(mat.size(),vector<int>(mat[0].size(),0));
         for(int i=0;i<mat.size();i++)
         {
             for(int j=0;j<mat[0].size();j++)
             {
                 if(mat[i][j]==0)
                 {
-                    sol[i][j]=0;
-                    q.push({i,j});
+                    q.push({i,j,0});
+                    vis[i][j]=1;
                 }
-              
             }
         }
-                   while(!q.empty())
-                    {
-                        int l=q.front()[0];
-                        int m=q.front()[1];
-                        // int wt=q.front()[2];
-                         q.pop();
-                        for(auto&x:pr)
-                        {
-                            int ll=l+x.first;
-                            int mm=m+x.second;
-
-                            if(ll>=0&&ll<mat.size()&&mm>=0&&mm<mat[0].size()&&sol[ll][mm]==-1)
-                            {
-                                sol[ll][mm]=sol[l][m]+1;
-                                q.push({ll,mm});
-                            }
-                             
-        
-                        }
-                        
-                    }//while
+        while(!q.empty())
+        {
+            auto x=q.front();
+            q.pop();
+            for(auto&y:pr)
+            {
+                int new_row=x[0]+y.first;
+                int new_col=x[1]+y.second;
+                if(new_row>=0&&new_row<mat.size()&&new_col>=0&&new_col<mat[0].size()&&vis[new_row][new_col]==0)
+                {
+                    vis[new_row][new_col]=1;
+                    sol[new_row][new_col]=x[2]+1;
+                    q.push({new_row,new_col,x[2]+1});
+                }
+            }
+        }
         return sol;
-        
     }
 };
