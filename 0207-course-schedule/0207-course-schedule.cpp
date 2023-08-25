432627189af035c743339ac543013d0fbe1018e6
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-25 20:39:22 +0530

    Time: 20 ms (77.35%), Space: 12.9 MB (96.50%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-   bool detectcycle(vector<vector<int>>&v,int src,vector<int>&rst,vector<int>&vis){
-        vis[src]=1;
-        rst[src]=1;
-        for(auto x:v[src]){
-            if(!vis[x]&&detectcycle(v,x,rst,vis)){
-                return true;
-            }
-            else if(rst[x]==1)
-            return true;
-        }
-        rst[src]=0;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>v(numCourses);
-        stack<int>s;
-        vector<int>vis(numCourses),rst(numCourses);
-        for(auto x: prerequisites){
-            v[x[1]].push_back(x[0]);
+        vector<int>indeg(numCourses,0);
+       vector<int>adj[numCourses];
+        for(auto&x:prerequisites)
+        {
+            adj[x[1]].push_back(x[0]);
+            indeg[x[0]]++;
         }
-        for(int i=0;i<numCourses;i++){
-           if(!vis[i])
-           if(detectcycle(v,i,rst,vis))
-           return false;
+        queue<int>q;
+        for(int i=0;i<indeg.size();i++)if(indeg[i]==0)q.push(i);
+        int c=0;                                             
+        while(!q.empty())
+        {
+            int ele=q.front();
+            q.pop();
+            c++;
+            for(auto&x:adj[ele])
+            {
+                indeg[x]--;
+                if(indeg[x]==0)q.push(x);
+            }
         }
-       
-        return true;
+        return c==numCourses?true:false;                                             
+        
     }
 };
