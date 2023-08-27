cf31b67a89017b6d7f7a192011eb00dd182cc0e9
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-28 01:06:20 +0530

    Time: 172 ms (51.45%), Space: 49.4 MB (49.96%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+        if(stones[1]!=1)return false;
+        if(stones.size()==2)return true;
+        int last=stones[stones.size()-1];
+        unordered_map<int ,set<int>>mpp;
+        for(auto&x:stones)
+        mpp.insert({x,{}});
+        mpp[1].insert({1,2});
+        for(int i=1;i<stones.size();i++)
+        {
+            for(auto&x:mpp[stones[i]])
+            {
+                if(mpp.find(stones[i]+x)!=mpp.end())
+                {
+                    if(stones[i]+x==last)return true;
+                    else
+                    {
+                        // if(x-1!=0)
+                        mpp[stones[i]+x].insert({x-1,x,x+1});
+                    }
+                }
+            }
+        }
+        
+        
+    
+        return false;
+    }
+};
