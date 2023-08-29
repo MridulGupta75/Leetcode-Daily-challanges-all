9e1125a8f41340dfb1a382770ffa99038d978d89
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-08-30 00:45:01 +0530

    Time: 414 ms (5.02%), Space: 85.1 MB (5.66%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int n=customers.size();
+        vector<int>dp(n+1);
+       int y=0,no=0;
+        
+        for(auto&x:customers)
+            if(x=='Y')y++;
+            else no++;
+        
+        map<int,pair<int,int>>mpp;
+        for(int i=0;i<=customers.size();i++)
+        {
+            mpp[i]={y,no};
+            if(customers[i]=='Y')y--;
+            else no--;
+        }
+        no=0;
+        for(int i=0;i<=customers.size();i++)
+        {
+            // auto x=mpp[i];
+            dp[i]=mpp[i].first+no;
+             if(customers[i]=='N')no++;
+            
+        }
+        int mini=dp[0],pos=0;
+        for(int i=1;i<=customers.size();i++)
+        {
+            if(dp[i]<mini)
+            {
+                mini=dp[i];
+                pos=i;
+            }
+        }
+        
+        
+        return pos;
+        
+        
+        
+    }
+};
