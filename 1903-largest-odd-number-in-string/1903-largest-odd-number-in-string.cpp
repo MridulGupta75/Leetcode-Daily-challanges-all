59b49fd65c2c3f0e34c6ef2c962c0d1bac3744b6
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-02 02:14:34 +0530

    Time: 26 ms (67.09%), Space: 15.1 MB (21.77%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,10 +4,7 @@ public:
         int j=num.size()-1;
         while(j>=0)
         {
-            if((num[j]-'0')%2!=0)
-            {
-                return num.substr(0,j+1);
-            }
+            if((num[j]-'0')%2!=0) return num.substr(0,j+1);
             j--;
         }
         return "";
