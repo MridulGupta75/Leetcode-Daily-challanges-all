bfb657f63aaa393d2f0dcfc2024f0825eb3c89f1
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-05 19:47:13 +0530

    Time: 103 ms (5.07%), Space: 160.7 MB (5.00%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,19 +1,38 @@
 class Solution {
+    private:
+    vector<string>vec;
+    bool check(string&str)
+    {
+        stack<char>st;
+        for(auto&x:str)
+        {
+            if(st.empty()&&x==')')return false;
+            if(x=='(')st.push(x);
+            else st.pop();
+        }
+        if(st.empty())return true;
+        return false;
+    }
+    void func(int i,int n,string &str)
+    {
+        if(i==n*2)
+        {
+            if(check(str))
+            vec.push_back(str);
+            return ;
+        }
+        if(i>n*2)return ;
+        str+='(';
+        func(i+1,n,str);
+        str.pop_back();
+        str+=')';
+        func(i+1,n,str);
+         str.pop_back();
+    }
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string>sol;
-        addPar(sol,"",n,0);
-        return sol;
-        
-    }
-    void addPar(vector<string>&sol,string str,int left,int right)
-    {
-        if(left==0&&right==0)
-        {
-            sol.push_back(str);
-            return;
-        }
-        if(left>0){addPar(sol,str+"(",left-1,right+1);}
-        if(right>0){addPar(sol,str+")",left,right-1);}
+        string str="";
+        func(0,n,str);
+        return vec;
     }
 };
