b7219ee5cf57812ea7d61fa02287c13099f2fb95
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-05 17:20:50 +0530

    Time: 12 ms (52.22%), Space: 20.9 MB (42.52%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -10,21 +10,25 @@
  * };
  */
 class Solution {
+    bool flag=true;
     private:
      int maxDepth(TreeNode* root) {
         if(!root)return 0;
         int l=maxDepth(root->left);
         int r=maxDepth(root->right);
+         if(abs(l-r)>1)flag=false;
         return (1+max(l,r));
     }
 public:
     bool isBalanced(TreeNode* root) {
         if(!root)return true;
-        bool l=isBalanced(root->left);
-        bool m=isBalanced(root->right);
-        if(!l||!m)return false;
-        int lt=maxDepth(root->left);
-        int rt=maxDepth(root->right);
-        return abs(rt-lt)<=1?true:false;
+        // bool l=isBalanced(root->left);
+        // bool m=isBalanced(root->right);
+        // if(!l||!m)return false;
+        // int lt=maxDepth(root->left);
+        // int rt=maxDepth(root->right);
+        // return abs(rt-lt)<=1?true:false;
+        maxDepth(root);
+        return flag;
     }
 };
