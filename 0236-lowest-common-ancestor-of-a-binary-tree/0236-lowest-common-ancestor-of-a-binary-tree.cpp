6d191e8ab9d65c6bf6f750ad2444919a1ea371a9
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-07 16:02:19 +0530

    Time: 17 ms (41.95%), Space: 14.4 MB (25.25%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -13,8 +13,9 @@ public:
        if(!root||root==p||root==q)return root;
         TreeNode* left=lowestCommonAncestor(root->left,p,q);
         TreeNode* right=lowestCommonAncestor(root->right,p,q);
-      if(!left)return right;
-     if(!right)return left;
-        return root;
+        return !left?right:(!right?left:root);
+      // if(!left)return right;
+      // if(!right)return left;
+      //   return root;
     }
 };
