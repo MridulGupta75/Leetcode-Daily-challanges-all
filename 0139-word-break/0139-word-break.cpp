dc9286af5919a696ee4203ede67322e1e999e3ea
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-08 18:09:40 +0530

    Time: 23 ms (25.33%), Space: 14.3 MB (31.80%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,22 +1,22 @@
 class Solution {
     private:
     set<string>Dict_to_set;
-    bool solve(int index,int n,string&s,vector<vector<int>>&DP)
+    bool solve(int index,int n,string&s,vector<int>&DP)
     {
         if(index>n)return false;
         if(Dict_to_set.find(s.substr(index))!=Dict_to_set.end())return true;
-        if(DP[index][n]!=-1)return DP[index][n];
+        if(DP[index]!=-1)return DP[index];
         for(int i=index;i<=n;i++)
         {
            if(Dict_to_set.find(s.substr(index,i-index+1))!=Dict_to_set.end()&&solve(i+1,n,s,DP))
-               return DP[index][n]=true;
+               return DP[index]=true;
         }
-        return DP[index][n]=false;
+        return DP[index]=false;
     }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         for(auto&x:wordDict)Dict_to_set.insert(x);
-        vector<vector<int>>DP(s.size(),vector<int>(s.size(),-1));
+        vector<int>DP(s.size(),-1);
        return  solve(0,s.size()-1,s,DP);
     }
 };
