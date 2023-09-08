097b37ce2939ff040dfb5c5bbaa3ea3eabb987c8
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-08 16:03:29 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (6.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,20 +3,17 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>sol;
         
-        for(int i=0;i<numRows;i++)
+        sol.push_back({{1}});
+        for(int i=1;i<numRows;i++)
         {
-            vector<int>subsol(i+1,1);
-             // subsol.push_back(1);
+             vector<int>subsol;
+            subsol.push_back(1);
             for(int j=1;j<i;j++)
             {
-                int l=0,r=0;
-                // if(i>0&&j-1>=0)
-                    l=sol[i-1][j-1];
-              r=sol[i-1][j];
-                
-                subsol[j]=(l+r);
+                subsol.push_back(sol[i-1][j]+sol[i-1][j-1]);
             }
-            sol.push_back(subsol);
+               subsol.push_back(1);
+              sol.push_back(subsol);
         }
         return sol;
     }
