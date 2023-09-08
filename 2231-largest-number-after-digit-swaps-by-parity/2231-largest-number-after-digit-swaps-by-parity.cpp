3416e4209da58c3c9a29db711fd8e5039a019b6a
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-08 22:03:21 +0530

    Time: 3 ms (46.91%), Space: 6.1 MB (39.90%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -1,35 +1,14 @@
 class Solution {
+    private:
+    priority_queue<char>odd;
+    priority_queue<char>even;
 public:
     int largestInteger(int num) {
-        vector<int>odd;
-        vector<int>e;
-        string n=to_string(num);
-        for(auto&x:n)
-        {
-            if((x-'0')%2)
-                odd.push_back(x-'0');
-            else
-                e.push_back(x-'0');
-        }
-        sort(odd.begin(),odd.end(),greater<int>());
-        sort(e.begin(),e.end(),greater<int>());
-        int ans=0;
-        int i=0;
-        int j=0;
-    for(auto&x:n)
-    {
-        ans*=10;
-        if((x-'0')%2)
-        {
-           ans+=(odd[i++]);
-            
-        }
-        else
-        {
-            ans+=(e[j++]);
-           
-        }
-    }
-        return ans;
+        string str=to_string(num);
+        for(auto&x:str)if((x-'0')%2)even.push(x);else odd.push(x);
+        for(int i=0;i<str.size();i++)
+        if((str[i]-'0')%2) str[i]=even.top(),even.pop();
+        else  str[i]=odd.top(),odd.pop();
+        return stoi(str);
     }
 };
