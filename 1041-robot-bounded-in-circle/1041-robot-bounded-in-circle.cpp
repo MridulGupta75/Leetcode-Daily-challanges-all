4707453eaec69b92caef99826ee6e1097241434d
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-11 02:11:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (48.29%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -3,28 +3,35 @@ public:
     bool isRobotBounded(string commands) {
         int x = 0;
         int y = 0;
+                                      // N       S      E         W
+        vector<vector<int>>coordinates{{1, 0}, {-1, 0}, {0, 1}, {0,-1}};
         
-        vector<vector<int>>coordinates{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        
-        int direction = 0;
+             int dir=0;
         
         for(int i = 0; i < commands.size(); ++i) {
             if(commands[i] == 'L') {
-                direction = (direction + 3) % 4;
+                if(dir==0)dir=3;
+                else if(dir ==1)dir=2;
+                 else if(dir==2)dir=0;
+                else dir=1;
+                
             }
             else if(commands[i] == 'R') {
-                direction = (direction + 1) % 4;
+                if(dir==0)dir=2;
+                else if(dir==1)dir=3;
+                 else if(dir==2)dir=1;
+                else dir=0;
             }
             else {
-                x += coordinates[direction][0];
-                y += coordinates[direction][1];
+                x += coordinates[dir][0];
+                y += coordinates[dir][1];
             }
         }
         
        if((x == 0 & y == 0)) {
            return true;
        }
-        if(direction==0)return false;
+        if(dir==0)return false;
        
        return true;
     }
