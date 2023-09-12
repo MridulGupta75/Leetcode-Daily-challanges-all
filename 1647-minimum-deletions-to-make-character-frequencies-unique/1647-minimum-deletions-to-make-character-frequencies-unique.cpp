5b86b7ea4dbe04b77cc7a50fb5ebfdee1432eb85
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-13 00:42:27 +0530

    Time: 52 ms (71.58%), Space: 17.5 MB (37.86%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<char,int>mpp;
-        for(auto&x:s)mpp[x]++;
-        map<int,vector<char>,greater<int>>m;
-        for(auto&x:mpp) m[x.second].push_back(x.first);
-        mpp.clear();
+        // mera tarika
+        
+        
+        
+        // unordered_map<char,int>mpp;
+        // for(auto&x:s)mpp[x]++;
+        // map<int,vector<char>,greater<int>>m;
+        // for(auto&x:mpp) m[x.second].push_back(x.first);
+        // mpp.clear();
+        // int c=0;
+        // for(auto&x:m)
+        // {
+        //     if(x.first==0)return c;
+        //     int size=x.second.size();
+        //     if(size-1!=0) for(int i=1;i<size;i++,c++) m[x.first-1].push_back(x.second[i]);
+        // }
+        // return c;
+        
+        
+        // tarrika 2
+        
+        vector<int>freq(26,0);
+        for(auto&x:s) freq[x-'a']++;
+        set<int>st;
         int c=0;
-        for(auto&x:m)
+        for(int i=0;i<26;i++)
         {
-            if(x.first==0)return c;
-            int size=x.second.size();
-            if(size-1!=0) for(int i=1;i<size;i++,c++) m[x.first-1].push_back(x.second[i]);
+            
+            if(st.find(freq[i])==st.end())
+            {
+                st.insert(freq[i]);
+                continue;
+            }
+            while(freq[i]>0&&st.find(freq[i])!=st.end())
+            {
+                freq[i]--;
+                c++;
+            }
+            if(freq[i]>0) st.insert(freq[i]);
+            
         }
         return c;
     }
