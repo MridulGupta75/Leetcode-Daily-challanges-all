2629b95a82c8cce6600309e4f63437037cfe7e80
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-13 00:27:33 +0530

    Time: 67 ms (48.02%), Space: 17.9 MB (9.58%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        unordered_map<char,int>mpp;
+        for(auto&x:s)mpp[x]++;
+        map<int,vector<char>,greater<int>>m;
+        for(auto&x:mpp) m[x.second].push_back(x.first);
+        int c=0;
+        //   for(auto&x:m)
+        // {
+        //     cout<<x.first<<" ";
+        //     for(auto&y:x.second)
+        //         cout<<y<<" ";
+        //     cout<<endl;
+        // }
+        for(auto&x:m)
+        {
+            if(x.first==0)continue;
+            int size=x.second.size();
+            
+            if(size-1!=0)
+            {
+                for(int i=1;i<size;i++)
+                {
+                m[x.first-1].push_back(x.second[i]);
+                    c++;
+                }
+                    
+            }
+        }
+        return c;
+        
+        
+        
+    }
+};
