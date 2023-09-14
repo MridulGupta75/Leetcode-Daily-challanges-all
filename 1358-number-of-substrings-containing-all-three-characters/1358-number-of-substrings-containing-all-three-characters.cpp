9a724f0ffa6dcdd966c0433a30e2ac5fcd1a203a
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2023-09-14 18:21:09 +0530

    Time: 105 ms (5.76%), Space: 27.8 MB (5.51%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,8 +2,6 @@ class Solution {
     private:
     bool check(vector<int>v)
     {
-        // if(v[0]&&v[1]&&v[2]) return true;
-        // return false;
         if(v[0]==-1||v[1]==-1||v[2]==-1)return false;
         return true;
     }
@@ -15,7 +13,6 @@ public:
         int count =0;
         while(j<s.size())
         {
-            
             if(s[j]=='a')mp[0]=j;
             else if(s[j]=='b')mp[1]=j;
             else mp[2]=j;
