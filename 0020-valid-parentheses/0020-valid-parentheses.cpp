fd474e7452eedbcd0c8cdf746506b4e6c0fc43a9
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2024-10-20 18:31:44 +0530

    Time: 0 ms (100%), Space: 8 MB (29.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,14 +5,10 @@ public:
     bool isValid(string s) {
         for(auto&x:s)
         {
-            if(x=='('||x=='['||x=='{')
-            {
-                st.push(x);
-            }
+            if(x=='('||x=='['||x=='{') st.push(x);
             else
             {
-                if(st.empty())return false;
-                if(st.top()==x-2||st.top()==x-1) 
+                if(!st.empty()&&(st.top()==x-2||st.top()==x-1)) 
                 {
                     st.pop();
                     continue;
