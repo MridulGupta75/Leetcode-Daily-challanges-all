0309b97c51ac235379a2cf4096d1d1148a84546e
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2025-04-19 21:51:51 +0530

    Time: 0 ms (100%), Space: 29.8 MB (34.95%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,12 +4,11 @@ public:
          k=k%nums.size();
         if(nums.size()<=1||k<=0) return;
         int d=nums.size()-k;
-        int c=d-1;
         int temp[k];
         int cc=0;
         for(int i=d;i<nums.size();i++) temp[cc++]=nums[i];
-
-        for(int j=nums.size()-1;c>=0;j--) if(c>=0)nums[j]=nums[c--];
+        cc=d-1;
+        for(int j=nums.size()-1;cc>=0;j--) if(cc>=0)nums[j]=nums[cc--];
         for(int i=0;i<k;i++)nums[i]=temp[i];
         
     }
