e0562de34ac3c2f9c2beb15a734c84c5afa7dab9
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2025-04-19 22:06:08 +0530

    Time: 0 ms (100%), Space: 29.7 MB (37.03%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -18,9 +18,11 @@ public:
         // optimal
 
 
-        
-        reverse(nums.begin(),nums.end()-k);
+        k=k%nums.size();
+
         reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end()-k);
+        
         reverse(nums.begin(),nums.end());
     }
 };
