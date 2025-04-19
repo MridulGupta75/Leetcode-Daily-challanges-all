2ea9b0ffcfe856f09d6e174bb18cbbce3423d190
Mridul Gupta <66024896+MridulGupta75@users.noreply.github.com>
2025-04-19 22:05:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-         k=k%nums.size();
-        if(nums.size()<=1||k<=0) return;
-        int d=nums.size()-k;
-        int temp[k];
-        int cc=0;
-        for(int i=d;i<nums.size();i++) temp[cc++]=nums[i];
-        cc=d-1;
-        for(int j=nums.size()-1;cc>=0;j--) if(cc>=0)nums[j]=nums[cc--];
-        for(int i=0;i<k;i++)nums[i]=temp[i];
+
+        // brute aproach 
+        //  k=k%nums.size();
+        // if(nums.size()<=1||k<=0) return;
+        // int d=nums.size()-k;
+        // int temp[k];
+        // int cc=0;
+        // for(int i=d;i<nums.size();i++) temp[cc++]=nums[i];
+        // cc=d-1;
+        // for(int j=nums.size()-1;cc>=0;j--) if(cc>=0)nums[j]=nums[cc--];
+        // for(int i=0;i<k;i++)nums[i]=temp[i];
         
+
+
+        // optimal
+
+
+        
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
